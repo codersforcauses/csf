ddb7ae192586271fb4442eb45ea3b0c1b3ff0930
nicodeas <nicholas.hy.michlin@gmail.com>
2023-06-13 16:47:51 +0800

    environment init

--- server/api/settings.py
+++ server/api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e*-y0b8croe6^g4(o)7x3_t_pa2wq1r*1jj+eo$ue-q-i)%opm"
+SECRET_KEY = os.environ.get("API_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("APP_ENV") == "DEVELOPMENT" or False
 
 ALLOWED_HOSTS = []
 
