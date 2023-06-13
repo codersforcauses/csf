ebeb21ddce3bc0b682d21940a32494495ceb78c5
ChantelleKerr <chantellekerr93@gmail.com>
2023-06-13 21:49:37 +0800

    Add django superuser

--- docker/server/entrypoint.sh
+++ docker/server/entrypoint.sh
@@ -3,5 +3,10 @@
 echo "Applying database migrations"
 python manage.py migrate --noinput
 
+
+# Create Django Superuser
+echo "Creating Django Superuser"
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+
 echo "Starting server"
 python manage.py runserver 0.0.0.0:8081
