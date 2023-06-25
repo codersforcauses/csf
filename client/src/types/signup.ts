47d490e64ea4128eb50684b5520cc24433cd26f0
benvarian <88700858+benvarian@users.noreply.github.com>
2023-06-25 11:03:27 +0800

    add signup type, link it to component, clean up basic files

---
+++ client/src/types/signup.ts
@@ -0,0 +1,13 @@
+export default class signup  {
+    username?: string;
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    confirmEmail?: string;
+    password?: string;
+    confirmPassword?: string;
+    avatar?: string;
+    teamSignup?: boolean;
+    hasConsent?: boolean;
+}
+
