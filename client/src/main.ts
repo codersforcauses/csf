ebbe0174cadf21033d7f78b7ed27f0cfe8a9a437
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2023-06-26 20:18:26 +0800

    fix prettier check errors

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,7 +4,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 // Vuetify
 import 'vuetify/styles'
-import { ThemeDefinition, createVuetify } from 'vuetify'
+import { createVuetify } from 'vuetify'
+import type { ThemeDefinition } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import * as icons from 'vuetify/iconsets/mdi-svg'
@@ -17,12 +18,12 @@ const app = createApp(App)
 const csfCustomTheme: ThemeDefinition = {
   dark: false,
   colors: {
-    red: "rgb(237, 28, 36)",
-    black: "rgb(0, 0, 0)",
-    white: "rgb(255, 255, 255)",
-    green: "rgb(0, 157, 79)",
-    blue: "rgb(52, 94, 158)",
-    tint: "rgb(249, 241, 227)"
+    red: 'rgb(237, 28, 36)',
+    black: 'rgb(0, 0, 0)',
+    white: 'rgb(255, 255, 255)',
+    green: 'rgb(0, 157, 79)',
+    blue: 'rgb(52, 94, 158)',
+    tint: 'rgb(249, 241, 227)'
   }
 }
 
@@ -31,7 +32,7 @@ const vuetify = createVuetify({
   directives,
   theme: {
     themes: {
-      csfCustomTheme,
+      csfCustomTheme
     },
     defaultTheme: 'csfCustomTheme'
   },
