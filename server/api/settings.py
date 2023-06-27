93957d532dc72d8b0e6d70725eed31e2d6a2e99e
benvarian <88700858+benvarian@users.noreply.github.com>
2023-06-27 22:20:56 +0800

    more stupid tings

--- server/api/settings.py
+++ server/api/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "api.event",
-    "rest_framework"
+    "rest_framework",
     "rest_framework_simplejwt"
 ]
 
