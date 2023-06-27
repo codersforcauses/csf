f86bbea2b4792f02d1c5558fe45f0242dbeee8a2
benvarian <88700858+benvarian@users.noreply.github.com>
2023-06-27 21:50:46 +0800

    added serialiser, added proper views that do stuff, and rest_framework to settings.py and installed on own machine

---
+++ server/api/event/serializers.py
@@ -0,0 +1,8 @@
+from django.db.models.base import Model 
+from .models import Event
+from rest_framework.serializers import ModelSerializer
+
+class EventSerialiser(ModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
