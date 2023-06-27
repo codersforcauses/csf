2ea8f3c76af9675969e43aa66bf41fadfd62a2f9
benvarian <88700858+benvarian@users.noreply.github.com>
2023-06-27 22:32:42 +0800

    add extra request

--- server/api/event/views.py
+++ server/api/event/views.py
@@ -20,6 +20,11 @@ def get_event(request, event_id):
     print(serializer.data)
     return Response(serializer.data)
 
+@api_view(['GET'])
+def get_events(request):
+    events = Event.objects.all()
+    serializer = EventSerialiser(events, many=True)
+    return Response(serializer.data)
 
 @api_view(['PUT'])
 def update_event(request, event_id):
