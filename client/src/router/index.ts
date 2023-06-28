07860d7518cc0115ee2971c5ecede4e04f0cfb01
ChantelleKerr <chantellekerr93@gmail.com>
2023-06-28 17:45:55 +0800

    Added router link to homeview page

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SignupView from '../views/SignupView.vue'
 
+import HomeView from '../views/homeView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
