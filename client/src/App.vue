d87bcca254a7ccaf82faeaa961fb2da1f2017a50
wuryantoAji <ajiw4056@gmail.com>
2023-06-28 16:18:30 +0800

    rename from Navbar.vue to NavbarComponents.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import NavBar from './components/NavBar.vue'
+import NavbarComponents from './components/NavbarComponents.vue'
 import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <header>
-    <NavBar />
+    <NavbarComponents />
   </header>
   <RouterView />
 </template>
