5752c28973687228d065340be4b97816bca22dc6
wuryantoAji <ajiw4056@gmail.com>
2023-06-28 15:52:04 +0800

    change from option api to composition, discard toolbar title, remove padding from main.css, add routing to signup

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,11 +1,10 @@
 <template>
-  <img src="@/assets/Footer-min.jpeg" class="border"/>
+  <img id="app" src="@/assets/Footer-min.jpeg" class="border"/>
   <v-toolbar dark app color="black" class="hidden-sm-and-down mt-2">
     <img alt="CSF Logo"  src="@/assets/CSF_Logo_WHITE.png" width="125" height="60" />
-    <v-toolbar-title class="hidden-md-and-down">Community Spirit Foundation</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn v-for="item in menu" :key="item.icon" :to="item.link" flat>{{
+      <v-btn v-for="item in menu" :key="item.title" :to="item.link" flat>{{
         item.title
       }}</v-btn>
     </v-toolbar-items>
@@ -13,18 +12,18 @@
   <v-toolbar dark app color="black" class="hidden-md-and-up mt-2">
     <img alt="CSF Logo"  src="@/assets/CSF_Logo_WHITE.png" width="125" height="60" />
     <v-spacer></v-spacer>
-    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-up-transition">
+    <v-dialog v-model="dialog" :fullscreen="mobile" transition="dialog-up-transition">
       <template v-slot:activator="{ props }" >
-        <button v-bind="props" class="hidden-md-and-up">
-          <i :class="'fa fa-bars  fa-lg'"></i>
-        </button>
+        <v-btn icon v-bind="props" class="hidden-md-and-up">
+          <v-icon :icon="menuBarIcon"></v-icon>
+        </v-btn>
       </template>
       <v-card class="overall mt-2">
         <v-toolbar dark app color="black" class="mt-2">
           <img alt="CSF Logo"  src="@/assets/CSF_Logo_WHITE.png" width="125" height="60" />
           <v-spacer></v-spacer>
           <v-btn icon @click.native="dialog = false" class="hidden-md-and-up">
-            <i :class="'fa fa-times fa-lg'"></i>
+            <v-icon :icon="closeMenuBarIcon"></v-icon>
           </v-btn>
         </v-toolbar>
         <v-list class="listItem"> 
@@ -32,6 +31,8 @@
             v-for="(item, index) in menu"
             :key="index"
             :value="index"
+            :to="item.link"
+            @click.native="dialog = false"
           >
             <template v-slot:prepend>
               <v-icon v-if="item.icon">{{item.icon}}</v-icon>
@@ -44,33 +45,28 @@
   </v-toolbar>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      menu: [
-        { icon: "fa fa-address-card  fa-lg", title: "About Us" },
-        { icon: "fa fa-chart-line  fa-lg", title: "Dashboard" },
-        { icon: "fa fa-trophy  fa-lg", title: "Challenges" },
-        { icon: "fa fa-users  fa-lg", title: "Team Page" },
-        { icon: "fa fa-calendar  fa-lg", title: "Events" },
-        { icon: "fa fa-star  fa-lg", title: "Leaderboards" },
-        { icon: "fa fa-sign-in-alt  fa-lg", title: "Login" },
-        { icon: "fa fa-pen-square  fa-lg", title: "Sign Up" },
-      ],
-      dialog: false
-    };
-  },
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useDisplay } from 'vuetify'
+import { aliases } from 'vuetify/iconsets/mdi-svg';
 
-  methods: {
-    menuItems() {
-      return this.menu;
-    },
-  }
-};
+const { mobile } = useDisplay()
+const dialog = ref(false)
+const menu = [
+        { icon: "fa fa-address-card", title: "About Us", link: "/" },
+        { icon: "fa fa-chart-line", title: "Dashboard", link: "/"  },
+        { icon: "fa fa-trophy", title: "Challenges", link: "/"  },
+        { icon: "fa fa-users", title: "Team Page", link: "/"  },
+        { icon: "fa fa-calendar", title: "Events", link: "/"  },
+        { icon: "fa fa-star", title: "Leaderboards", link: "/"  },
+        { icon: "fa fa-sign-in-alt", title: "Login", link: "/"  },
+        { icon: "fa fa-pen-square", title: "Sign Up", link: "/signup"  },
+]
+const menuBarIcon = aliases.menu
+const closeMenuBarIcon = aliases.close
 </script>
 <style>
-@import url('https://use.fontawesome.com/releases/v5.9.0/css/all.css');
+@import url('https://use.fontawesome.com/releases/v5.15.4/css/all.css');
 .hidden-md-and-up {
   padding-right: 3%;
 }
@@ -82,11 +78,12 @@ export default {
   background-color: black;
 }
 .border {
-  height: 10px;
+  height: 100%;
   width: 100%;
+  max-height: 15px;
+  max-width: 1280px;
   overflow: hidden;
   position: absolute;
   top: 0;
-  left: 0;
 }
 </style>
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import SignupView from '../views/SignupView.vue'
+import AboutView from '../views/AboutView.vue'
+import SignUpView from '../views/SignUpView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'signup',
-      component: SignupView
+      name: 'about',
+      component: AboutView
+    },
+    {
+      path: '/signup',
+      name: 'signUp',
+      component: SignUpView
     }
   ]
 })
