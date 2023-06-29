cbb05935bb2573c68106243184fcd4fb3eb51e21
wuryantoAji <ajiw4056@gmail.com>
2023-06-29 13:24:12 +0800

    attempt to run prettier for navbarComponents and main.css

--- client/src/components/NavbarComponents.vue
+++ client/src/components/NavbarComponents.vue
@@ -1,18 +1,21 @@
 <template>
   <v-toolbar dark app color="black" class="hidden-sm-and-down">
-    <img alt="CSF Logo"  src="@/assets/CSF_Logo_WHITE.png" width="125" height="60" />
+    <img alt="CSF Logo" src="@/assets/CSF_Logo_WHITE.png" width="125" height="60" />
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn v-for="item in menu" :key="item.title" :to="item.link" flat>{{
-        item.title
-      }}</v-btn>
+      <v-btn v-for="item in menu" :key="item.title" :to="item.link" flat>{{ item.title }}</v-btn>
     </v-toolbar-items>
   </v-toolbar>
   <v-toolbar dark app color="black" class="hidden-md-and-up">
-    <img alt="CSF Logo"  src="@/assets/CSF_Logo_WHITE.png" width="125" height="60" />
+    <img alt="CSF Logo" src="@/assets/CSF_Logo_WHITE.png" width="125" height="60" />
     <v-spacer></v-spacer>
-    <v-dialog v-model="dialog" :fullscreen="mobile" transition="dialog-up-transition" class="hidden-md-and-up">
-      <template v-slot:activator="{ props }" >
+    <v-dialog
+      v-model="dialog"
+      :fullscreen="mobile"
+      transition="dialog-up-transition"
+      class="hidden-md-and-up"
+    >
+      <template v-slot:activator="{ props }">
         <v-btn icon v-bind="props" class="hidden-md-and-up menuIcon">
           <v-icon>mdi-menu</v-icon>
         </v-btn>
@@ -20,7 +23,7 @@
       <v-card class="overall">
         <v-container class="pa-0 ma-0">
           <v-toolbar dark app color="black" class="hidden-md-and-up">
-            <img alt="CSF Logo"  src="@/assets/CSF_Logo_WHITE.png" width="125" height="60" />
+            <img alt="CSF Logo" src="@/assets/CSF_Logo_WHITE.png" width="125" height="60" />
             <v-spacer></v-spacer>
             <v-btn icon @click="dialog = false" class="hidden-md-and-up menuIcon">
               <v-icon>mdi-close</v-icon>
@@ -28,9 +31,9 @@
           </v-toolbar>
         </v-container>
         <v-container class="pt-0 pl-0 pb-2 pr-0 border-container">
-          <img id="app" src="@/assets/Footer-min.jpeg" class="border hidden-md-and-up"/>
+          <img id="app" src="@/assets/Footer-min.jpeg" class="border hidden-md-and-up" />
         </v-container>
-        <v-list class="listItem"> 
+        <v-list class="listItem">
           <v-list-item
             v-for="(item, index) in menu"
             :key="index"
@@ -39,16 +42,16 @@
             @click="dialog = false"
           >
             <template v-slot:prepend>
-              <v-icon v-if="item.icon">{{item.icon}}</v-icon>
+              <v-icon v-if="item.icon">{{ item.icon }}</v-icon>
             </template>
-            <v-list-item-title >{{ item.title }}</v-list-item-title>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item>
         </v-list>
       </v-card>
     </v-dialog>
   </v-toolbar>
-  <img id="app" src="@/assets/Footer-min.jpeg" class="border hidden-sm-and-down"/>
-  <img id="app" src="@/assets/Footer-min.jpeg" class="border hidden-md-and-up"/>
+  <img id="app" src="@/assets/Footer-min.jpeg" class="border hidden-sm-and-down" />
+  <img id="app" src="@/assets/Footer-min.jpeg" class="border hidden-md-and-up" />
 </template>
 
 <script setup lang="ts">
@@ -58,14 +61,14 @@ import { useDisplay } from 'vuetify'
 const { mobile } = useDisplay()
 const dialog = ref(false)
 const menu = [
-        { icon: "mdi-card-account-details-outline", title: "About Us", link: "/" },
-        { icon: "mdi-chart-bar", title: "Dashboard", link: "/"  },
-        { icon: "mdi-trophy", title: "Challenges", link: "/"  },
-        { icon: "mdi-account-group", title: "Team Page", link: "/"  },
-        { icon: "mdi-calendar", title: "Events", link: "/"  },
-        { icon: "mdi-star", title: "Leaderboards", link: "/"  },
-        { icon: "mdi-login", title: "Login", link: "/"  },
-        { icon: "mdi-pencil-box", title: "Sign Up", link: "/signup"  },
+  { icon: 'mdi-card-account-details-outline', title: 'About Us', link: '/' },
+  { icon: 'mdi-chart-bar', title: 'Dashboard', link: '/' },
+  { icon: 'mdi-trophy', title: 'Challenges', link: '/' },
+  { icon: 'mdi-account-group', title: 'Team Page', link: '/' },
+  { icon: 'mdi-calendar', title: 'Events', link: '/' },
+  { icon: 'mdi-star', title: 'Leaderboards', link: '/' },
+  { icon: 'mdi-login', title: 'Login', link: '/' },
+  { icon: 'mdi-pencil-box', title: 'Sign Up', link: '/signup' }
 ]
 </script>
 
@@ -74,11 +77,11 @@ const menu = [
 .menuIcon {
   padding-right: 3%;
 }
-.listItem{
+.listItem {
   color: white;
   background-color: black;
 }
-.overall{
+.overall {
   background-color: black;
 }
 .border {
@@ -88,10 +91,10 @@ const menu = [
   overflow: hidden;
   position: absolute;
 }
-.border-container{
+.border-container {
   position: relative;
 }
-.v-list{
-  padding-top:0px;
+.v-list {
+  padding-top: 0px;
 }
 </style>
