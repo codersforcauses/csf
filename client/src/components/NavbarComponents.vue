22102a3ebd76e0ac0c1a8843989a0e708aa06e0b
wuryantoAji <ajiw4056@gmail.com>
2023-06-29 15:54:29 +0800

    move image asset to the appropriate folder, fix and change navbar css to use vuetify inline instead

--- client/src/components/NavbarComponents.vue
+++ client/src/components/NavbarComponents.vue
@@ -1,13 +1,25 @@
 <template>
   <v-toolbar dark app color="black" class="hidden-sm-and-down">
-    <img alt="CSF Logo" src="@/assets/CSF_Logo_WHITE.png" width="125" height="60" />
+    <v-img
+      src="/images/CSF_Logo_WHITE.png"
+      width="125"
+      height="60"
+      max-width="125"
+      max-height="60"
+    ></v-img>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
       <v-btn v-for="item in menu" :key="item.title" :to="item.link" flat>{{ item.title }}</v-btn>
     </v-toolbar-items>
   </v-toolbar>
   <v-toolbar dark app color="black" class="hidden-md-and-up">
-    <img alt="CSF Logo" src="@/assets/CSF_Logo_WHITE.png" width="125" height="60" />
+    <v-img
+      src="/images/CSF_Logo_WHITE.png"
+      width="125"
+      height="60"
+      max-width="125"
+      max-height="60"
+    ></v-img>
     <v-spacer></v-spacer>
     <v-dialog
       v-model="dialog"
@@ -16,24 +28,36 @@
       class="hidden-md-and-up"
     >
       <template v-slot:activator="{ props }">
-        <v-btn icon v-bind="props" class="hidden-md-and-up menuIcon">
+        <v-btn icon v-bind="props" class="hidden-md-and-up pr-3">
           <v-icon>mdi-menu</v-icon>
         </v-btn>
       </template>
-      <v-card class="overall">
+      <v-card color="black">
         <v-container class="pa-0 ma-0">
           <v-toolbar dark app color="black" class="hidden-md-and-up">
-            <img alt="CSF Logo" src="@/assets/CSF_Logo_WHITE.png" width="125" height="60" />
+            <v-img
+              src="/images/CSF_Logo_WHITE.png"
+              width="125"
+              height="60"
+              max-width="125"
+              max-height="60"
+            ></v-img>
             <v-spacer></v-spacer>
-            <v-btn icon @click="dialog = false" class="hidden-md-and-up menuIcon">
+            <v-btn icon @click="dialog = false" class="hidden-md-and-up pr-3">
               <v-icon>mdi-close</v-icon>
             </v-btn>
           </v-toolbar>
         </v-container>
-        <v-container class="pt-0 pl-0 pb-2 pr-0 border-container">
-          <img id="app" src="@/assets/Footer-min.jpeg" class="border hidden-md-and-up" />
+        <v-container class="pa-0 border-container">
+          <v-img
+            src="/images/Footer-min.jpeg"
+            width="100%"
+            height="8"
+            class="hidden-md-and-up"
+            cover
+          ></v-img>
         </v-container>
-        <v-list class="listItem">
+        <v-list class="listItem pa-0">
           <v-list-item
             v-for="(item, index) in menu"
             :key="index"
@@ -72,30 +96,8 @@ const menu = [
 </script>
 
 <style>
-.menuIcon {
-  padding-right: 3%;
-}
 .listItem {
   color: white;
   background-color: black;
-}
-.overall {
-  background-color: black;
-}
-.border {
-  height: 100%;
-  width: 100%;
-  max-height: 10px;
-  overflow: hidden;
-  position: absolute;
-}
-.border-container {
-  position: relative;
-}
-.v-list {
-  padding-top: 0px;
-}
-.img {
-  height: 200px;
 }
 </style>
