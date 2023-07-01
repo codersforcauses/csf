eaf5f08c08dececceac20315a5cbe98fcd61c170
HuxleyBerry <huxley.berry@gmail.com>
2023-07-01 14:23:48 +0800

    add types and access theme colours in a better way

--- client/src/components/Event.vue
+++ client/src/components/Event.vue
@@ -4,28 +4,27 @@
             <span>{{ name }}</span>
             <v-icon v-if="canEdit" icon="mdi-pencil"></v-icon>
             <v-spacer></v-spacer>
-            <v-chip variant="outlined" :class="{ 'private-chip': isPrivate, 'official-chip': !isPrivate }">{{ isPrivate ? "Private" : "Official" }}</v-chip>
+            <v-chip variant="outlined" :class="{ 'text-secondaryGreen': isPrivate, 'text-secondaryBlue': !isPrivate }">{{ isPrivate ? "Private" : "Official" }}</v-chip>
         </v-card-title>
-        <v-card-subtitle>{{ startDate }} - {{ endDate }}</v-card-subtitle>
+        <v-card-subtitle class="text-primaryRed">{{ startDate }} - {{ endDate }}</v-card-subtitle>
         <v-card-text>{{ description }}</v-card-text>
     </v-card>
 </template>
 
 <script setup lang="ts">
-    defineProps(['name','startDate','endDate','description','canEdit', 'isPrivate'])
+    interface Props {
+        name: string
+        startDate: string
+        endDate: string
+        description: string
+        canEdit: boolean
+        isPrivate: boolean
+    }
+    defineProps<Props>()
 </script>
 
 <style>
-    .v-card-subtitle {
-        color: rgb(var(--v-theme-primaryRed));
-    }
     .v-card-title {
         display: flex;
-    }
-    .official-chip {
-        color: rgb(var(--v-theme-secondaryGreen));
-    }
-    .private-chip {
-        color: rgb(var(--v-theme-secondaryBlue));
     }
 </style>
--- client/src/views/EventsView.vue
+++ client/src/views/EventsView.vue
@@ -1,8 +1,8 @@
 
 <template>
-  <v-toolbar>
+  <v-toolbar class="bg-primaryWhite">
     <v-text-field prepend-inner-icon="mdi-magnify" hide-details label="Search Events" clearable v-model="searchQuery"/>
-    <v-btn v-if="tempIsTeamAdmin" icon="mdi-plus" size="small" id="add-event-btn"></v-btn>
+    <v-btn v-if="tempIsTeamAdmin" icon="mdi-plus" size="small" id="add-event-btn" class="bg-primaryRed text-primaryWhite"></v-btn>
   </v-toolbar>
   
   <Event v-for="event in filteredEventsList" :name="event.name" :startDate="event.startDate" :endDate="event.endDate" :description="event.description" :canEdit="tempIsTeamAdmin && event.isPrivate" :isPrivate="event.isPrivate"/>
@@ -11,29 +11,33 @@
 <script setup lang="ts">
   import Event from '../components/Event.vue'
   import { ref, computed } from 'vue'
-  const temporaryEventsList = ref([
+
+  interface Event {
+    name: string
+    startDate: string
+    endDate: string
+    description: string
+    isPrivate: boolean
+  }
+
+  const temporaryEventsList = ref<Event[]>([
     {name: "Run Very Far", startDate: "17 Nov", endDate: "23 Nov", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", isPrivate: false},
     {name: "Run Even Further", startDate: "25 Nov", endDate: "2 Dec", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", isPrivate: true},
     {name: "Walk", startDate: "6 Dec", endDate: "6 Dec", description: "Designers use Lorem Ipsum as a dummy text, something to cover the fact that content is missing from the wireframe. “Lorem Ipsum” is followed by more Latin text, making it easy for users or other designers to ignore it and imagine something more familiar or relevant in its place.", isPrivate: true},
   ])
-  const searchQuery = ref("")
-  const filteredEventsList = computed(() => {
-    let query = searchQuery.value ? searchQuery.value.toLowerCase() : ""
+  const searchQuery = ref<string>("")
+  const filteredEventsList = computed<Event[]>(() => {
+    let query: string = searchQuery.value ? searchQuery.value.toLowerCase() : ""
     return temporaryEventsList.value.filter( e => e.name.toLowerCase().includes(query) || e.description.toLowerCase().includes(searchQuery.value))
   })
-  const tempIsTeamAdmin = ref(true)
+  const tempIsTeamAdmin = ref<boolean>(true)
 
 </script> 
 
 <style>
   #add-event-btn {
-    background-color: rgb(var(--v-theme-primaryRed));
-    color: rgb(var(--v-theme-primaryWhite));
     font-size: 20px;
     margin-right: 20px;
-  }
-  .v-toolbar {
-    background-color: rgb(var(--v-theme-primaryWhite));
   }
   .v-text-field {
     margin: 20px;
