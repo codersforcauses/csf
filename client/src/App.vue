5419b8188d9f1aad468e5ad5a3040003a554aea9
Blizzerac <TBlizzerac@hotmail.com>
2023-07-02 00:18:57 +0800

    App.vue formatted to prettier standard

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,7 +10,6 @@ import { RouterView } from 'vue-router'
   <RouterView />
 </template>
 
-
 <style>
 @font-face {
   font-family: 'Hackney';
@@ -19,105 +18,121 @@ import { RouterView } from 'vue-router'
 
 @font-face {
   font-family: 'Sofia Pro';
-  src: local('Sofia Pro'), url('./assets/fonts/Mostardesign - Sofia Pro Black italic.otf') format('opentype');
+  src: local('Sofia Pro'),
+    url('./assets/fonts/Mostardesign - Sofia Pro Black italic.otf') format('opentype');
   font-weight: 900;
   font-style: italic;
 }
 
 @font-face {
   font-family: 'Sofia Pro';
-  src: local('Sofia Pro'), url('./assets/fonts/Mostardesign - Sofia Pro Black.otf') format('opentype');
+  src: local('Sofia Pro'),
+    url('./assets/fonts/Mostardesign - Sofia Pro Black.otf') format('opentype');
   font-weight: 900;
 }
 
 @font-face {
   font-family: 'Sofia Pro';
-  src: local('Sofia Pro'), url('./assets/fonts/Mostardesign - Sofia Pro Bold italic.otf') format('opentype');
+  src: local('Sofia Pro'),
+    url('./assets/fonts/Mostardesign - Sofia Pro Bold italic.otf') format('opentype');
   font-weight: 700;
   font-style: italic;
 }
 
 @font-face {
   font-family: 'Sofia Pro';
-  src: local('Sofia Pro'), url('./assets/fonts/Mostardesign - Sofia Pro Bold.otf') format('opentype');
+  src: local('Sofia Pro'),
+    url('./assets/fonts/Mostardesign - Sofia Pro Bold.otf') format('opentype');
   font-weight: 700;
 }
 
 @font-face {
   font-family: 'Sofia Pro';
-  src: local('Sofia Pro'), url('./assets/fonts/Mostardesign - Sofia Pro Extra Light italic.otf') format('opentype');
+  src: local('Sofia Pro'),
+    url('./assets/fonts/Mostardesign - Sofia Pro Extra Light italic.otf') format('opentype');
   font-weight: 200;
   font-style: italic;
 }
 
 @font-face {
   font-family: 'Sofia Pro';
-  src: local('Sofia Pro'), url('./assets/fonts/Mostardesign - Sofia Pro Extra Light.otf') format('opentype');
+  src: local('Sofia Pro'),
+    url('./assets/fonts/Mostardesign - Sofia Pro Extra Light.otf') format('opentype');
   font-weight: 200;
 }
 
 @font-face {
   font-family: 'Sofia Pro';
-  src: local('Sofia Pro'), url('./assets/fonts/Mostardesign - Sofia Pro Light italic.otf') format('opentype');
+  src: local('Sofia Pro'),
+    url('./assets/fonts/Mostardesign - Sofia Pro Light italic.otf') format('opentype');
   font-weight: 300;
   font-style: italic;
 }
 
 @font-face {
   font-family: 'Sofia Pro';
-  src: local('Sofia Pro'), url('./assets/fonts/Mostardesign - Sofia Pro Light.otf') format('opentype');
+  src: local('Sofia Pro'),
+    url('./assets/fonts/Mostardesign - Sofia Pro Light.otf') format('opentype');
   font-weight: 300;
 }
 
 @font-face {
   font-family: 'Sofia Pro';
-  src: local('Sofia Pro'), url('./assets/fonts/Mostardesign - Sofia Pro Medium italic.otf') format('opentype');
+  src: local('Sofia Pro'),
+    url('./assets/fonts/Mostardesign - Sofia Pro Medium italic.otf') format('opentype');
   font-weight: 500;
   font-style: italic;
 }
 
 @font-face {
   font-family: 'Sofia Pro';
-  src: local('Sofia Pro'), url('./assets/fonts/Mostardesign - Sofia Pro Medium.otf') format('opentype');
+  src: local('Sofia Pro'),
+    url('./assets/fonts/Mostardesign - Sofia Pro Medium.otf') format('opentype');
   font-weight: 500;
 }
 
 @font-face {
   font-family: 'Sofia Pro';
-  src: local('Sofia Pro'), url('./assets/fonts/Mostardesign - Sofia Pro Regular italic.otf') format('opentype');
+  src: local('Sofia Pro'),
+    url('./assets/fonts/Mostardesign - Sofia Pro Regular italic.otf') format('opentype');
   font-weight: 400;
   font-style: italic;
 }
 
 @font-face {
   font-family: 'Sofia Pro';
-  src: local('Sofia Pro'), url('./assets/fonts/Mostardesign - Sofia Pro Regular.otf') format('opentype');
+  src: local('Sofia Pro'),
+    url('./assets/fonts/Mostardesign - Sofia Pro Regular.otf') format('opentype');
   font-weight: 400;
 }
 
 @font-face {
   font-family: 'Sofia Pro';
-  src: local('Sofia Pro'), url('./assets/fonts/Mostardesign - Sofia Pro Semi Bold italic.otf') format('opentype');
+  src: local('Sofia Pro'),
+    url('./assets/fonts/Mostardesign - Sofia Pro Semi Bold italic.otf') format('opentype');
   font-weight: 600;
   font-style: italic;
 }
 
 @font-face {
   font-family: 'Sofia Pro';
-  src: local('Sofia Pro'), url('./assets/fonts/Mostardesign - Sofia Pro Semi Bold.otf') format('opentype');
+  src: local('Sofia Pro'),
+    url('./assets/fonts/Mostardesign - Sofia Pro Semi Bold.otf') format('opentype');
   font-weight: 600;
 }
 
 @font-face {
   font-family: 'Sofia Pro';
-  src: local('Sofia Pro'), url('./assets/fonts/Mostardesign - Sofia Pro Ultra Light italic.otf') format('opentype');
+  src: local('Sofia Pro'),
+    url('./assets/fonts/Mostardesign - Sofia Pro Ultra Light italic.otf') format('opentype');
   font-weight: 100;
   font-style: italic;
 }
 
 @font-face {
   font-family: 'Sofia Pro';
-  src: local('Sofia Pro'), url('./assets/fonts/Mostardesign - Sofia Pro Ultra Light.otf') format('opentype');
+  src: local('Sofia Pro'),
+    url('./assets/fonts/Mostardesign - Sofia Pro Ultra Light.otf') format('opentype');
   font-weight: 100;
 }
 </style>
