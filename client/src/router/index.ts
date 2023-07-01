368e7a4732b98081a06f2bf697dd3ecbf6945127
benvarian <88700858+benvarian@users.noreply.github.com>
2023-07-01 20:43:36 +0800

    fix merge conflicts

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -3,7 +3,6 @@ import HomeView from '../views/HomeView.vue'
 
 import LoginView from '../views/LoginView.vue'
 
-import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,11 +17,6 @@ const router = createRouter({
       name: 'login',
       component: LoginView
     },
-    {
-      path: '/signup',
-      name: 'signup',
-      component: SignupView
-    }
   ]
 })
 
