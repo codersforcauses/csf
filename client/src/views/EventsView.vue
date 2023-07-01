645ed6bce87f9451b9f3a553092969e2c6603867
HuxleyBerry <huxley.berry@gmail.com>
2023-07-01 12:36:02 +0800

    events are filtered by contents of the search

--- client/src/views/EventsView.vue
+++ client/src/views/EventsView.vue
@@ -1,21 +1,26 @@
 
 <template>
   <v-toolbar>
-    <v-text-field prepend-inner-icon="mdi-magnify" hide-details label="Search Events" />
+    <v-text-field prepend-inner-icon="mdi-magnify" hide-details label="Search Events" v-model="searchQuery"/>
     <v-btn v-if="tempIsTeamAdmin" icon="mdi-plus" size="small" id="add-event-btn"></v-btn>
   </v-toolbar>
   
-  <Event v-for="event in temporaryEventsList" :name="event.name" :startDate="event.startDate" :endDate="event.endDate" :description="event.description" :canEdit="tempIsTeamAdmin && event.isPrivate" :isPrivate="event.isPrivate"/>
+  <Event v-for="event in filteredEventsList" :name="event.name" :startDate="event.startDate" :endDate="event.endDate" :description="event.description" :canEdit="tempIsTeamAdmin && event.isPrivate" :isPrivate="event.isPrivate"/>
 </template>
 
 <script setup lang="ts">
   import Event from '../components/Event.vue'
-  import { ref } from 'vue'
-  const temporaryEventsList = [
+  import { ref, computed } from 'vue'
+  const temporaryEventsList = ref([
     {name: "Run Very Far", startDate: "17 Nov", endDate: "23 Nov", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", isPrivate: false},
     {name: "Run Even Further", startDate: "25 Nov", endDate: "2 Dec", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", isPrivate: true},
     {name: "Walk", startDate: "6 Dec", endDate: "6 Dec", description: "Designers use Lorem Ipsum as a dummy text, something to cover the fact that content is missing from the wireframe. “Lorem Ipsum” is followed by more Latin text, making it easy for users or other designers to ignore it and imagine something more familiar or relevant in its place.", isPrivate: true},
-  ]
+  ])
+  const searchQuery = ref("")
+  const filteredEventsList = computed(() => {
+    let query = searchQuery.value.toLowerCase()
+    return temporaryEventsList.value.filter( e => e.name.toLowerCase().includes(query) || e.description.toLowerCase().includes(searchQuery.value))
+  })
   const tempIsTeamAdmin = ref(true)
 
 </script> 
