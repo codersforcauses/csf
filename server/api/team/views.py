e1ea3ce5c8c44472870fe570e2ac3f8f9354996d
YunnTun <22676427@student.uwa.edu.au>
2023-07-01 16:45:49 +0800

    fix small issues after first PR

--- server/api/team/views.py
+++ server/api/team/views.py
@@ -30,20 +30,14 @@ def get_teams(request):
 @api_view(['PUT'])
 def update_team(request, team_id):
     team = Team.objects.get(team_id=team_id)
-    if team.is_public is False:
-        serializer = TeamSerialiser(instance=team, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
+    serializer = TeamSerialiser(instance=team, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
         return Response(serializer.data)
-    else:
-        return Response("Team is not private")
 
 
 @api_view(['DELETE'])
 def delete_team(request, team_id):
     team = Team.objects.get(team_id=team_id)
-    if team.is_public is False:
-        team.delete()
-        return Response("Team successfully deleted")
-    else:
-        return Response("Team is not private")
+    team.delete()
+    return Response("Team successfully deleted")
