203bc3da2159693e2eb99d8aaca2aceced21059e
wuryantoAji <ajiw4056@gmail.com>
2023-07-03 16:44:14 +0800

    revert settings

--- server/api/settings.py
+++ server/api/settings.py
@@ -20,16 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# Dont forget to revert
-SECRET_KEY = os.environ.get("API_SECRET_KEY") or "supersecretkey"
+SECRET_KEY = os.environ.get("API_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("APP_ENV") == "DEVELOPMENT"
-
-# Dont forget to revert
 ALLOWED_HOSTS = (
-    ".localhost 127.0.0.1 [::1]".split()
-    if ".localhost 127.0.0.1 [::1]"
+    os.environ.get("API_ALLOWED_HOSTS").split()
+    if os.environ.get("API_ALLOWED_HOSTS")
     else []
 )
 
