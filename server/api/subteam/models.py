9df642a1b84e9d0feda64a404fb47ef48ce49dd9
YunnTun <22676427@student.uwa.edu.au>
2023-07-03 19:15:06 +0800

    fix linting 1

--- server/api/subteam/models.py
+++ server/api/subteam/models.py
@@ -5,7 +5,7 @@ from api.team.models import Team
 class SubTeam(models.Model):
     subteam_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=254)
-    team_id = models.ForeignKey(Team, on_delete=models.CASCADE) 
+    team_id = models.ForeignKey(Team, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name}"
