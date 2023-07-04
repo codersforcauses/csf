6304ac76b32fdd5db2870f0343f1e191211de43d
John Smith <makon1234@users.noreply.github.com>
2023-07-05 02:49:16 +0800

    update serializer and view

--- server/api/users/serializers.py
+++ server/api/users/serializers.py
@@ -5,4 +5,22 @@ from . import User
 class SignUpmodelSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email']
+        fields = ['first_name', 'last_name', 'email', 'password', 'confirm_password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def save(self):
+        user = User(
+            first_name=self.validated_data['first_name'],
+            last_name=self.validated_data['last_name'],
+            email=self.validated_data['email'],
+        )
+        password = self.validated_data['password']
+        confirm_password = self.validated_data['confirm_password']
+
+        if password != confirm_password:
+            raise serializers.ValidationError({'password': 'The Password do not match.'})
+        user.set_password(password)
+        user.save()
+        return user
--- server/api/users/views.py
+++ server/api/users/views.py
@@ -1,17 +1,30 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from api.users.serializers import SignUpmodelSerializer
 
-@api_view(['POST'])
+@api_view(['POST',])
 def register(request):
-    if request.method == 'POST':
-        data = request.body
-        firstname = data.get('firstname')
-        lastname = data.get('lastname')
-        password = data.get('password')
-        email = data.get('email')
-        print(firstname, lastname, password, email)
-        return Response('User registered successfully')
+
+    serializer = SignUpmodelSerializer(data=request.data)
+    data = {}
+    if serializer.is_valid():
+        account = serializer.save()
     else:
-        return Response({'error': 'Method Not Allowed'}, status=405)
+        data = serializer.errors
+    return Response(data)
+
+# @api_view(['POST'])
+# def register(request):
+#     if request.method == 'POST':
+#         data = request.body
+#         firstname = data.get('firstname')
+#         lastname = data.get('lastname')
+#         password = data.get('password')
+#         email = data.get('email')
+#         print(firstname, lastname, password, email)
+#         return Response('User registered successfully')
+#     else:
+#         return Response({'error': 'Method Not Allowed'}, status=405)
