386a7e8d6ddde7ee0e37bfc5ec6098ab6280caba
John Smith <makon1234@users.noreply.github.com>
2023-07-04 21:01:41 +0800

    added serializer.py file and merged main into branch

---
+++ server/api/users/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+ 
+from . import User
+
+class signup_modelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
