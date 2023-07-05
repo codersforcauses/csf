635c11ab3109c9886da4cdaf84852df361012691
K-Straiton <straiton98@gmail.com>
2023-07-05 13:54:54 +0800

    Add basic milleage display

--- client/src/views/DashboardView.vue
+++ client/src/views/DashboardView.vue
@@ -1,32 +1,28 @@
 <template>
-    <v-row align="center">
+    <v-row class="ma-0 pl-4 pr-4 pt-4 pb-0" align="center">
         <v-col>
             <v-card variant="flat">
-                <v-card-title>Welcome back, {{ tempUserFirstName }}!</v-card-title>
+                <v-card-title class="pa-0">Welcome back, {{ tempUserFirstName }}!</v-card-title>
             </v-card>
         </v-col>
     </v-row>
     <v-divider/>
-    <v-row align="center">
+    <v-row class="ma-0 pl-4 pr-4 pb-0 pt-0" align="center">
         <v-col>
-            <v-card variant="flat">
+            <v-card variant="outlined">
                 <v-row>
-                    <v-col>
-                        <v-icon icon="mdi-run"/>
+                    <v-col cols="auto">
+                        <v-icon icon="mdi-run-fast" size="48"/>
                     </v-col>
                     <v-col>
-                        <v-card-title style="background-color: rgba(0, 157, 79, 0.19);">{{ tempUserMileage }} KM</v-card-title>
+                        <v-chip color="green" class="rounded text-h6">{{ tempUserMileage }} KM</v-chip>
                         <v-card-subtitle>TOTAL</v-card-subtitle>
                     </v-col>
+                    <v-spacer/>
+                    <v-col>
+                        ADD
+                    </v-col>
                 </v-row>
-            </v-card>
-        </v-col>
-    </v-row>
-    <v-divider/>
-    <v-row align="center">
-        <v-col>
-            <v-card variant="flat">
-                <v-card-title>Mileage Graph</v-card-title>
             </v-card>
         </v-col>
     </v-row>
@@ -38,8 +34,6 @@
     const tempIsLoggedIn = ref(true)
     const tempUserFirstName = ref("John")
     const tempUserMileage = ref(100)
-
-
 </script>
 
 <style scoped></style>
