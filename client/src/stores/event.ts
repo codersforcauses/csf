bebdd9515e39dc6af6d0782c4a377abe9105c492
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2023-07-05 19:52:18 +0800

    cleanup / finish `event.ts`

--- client/src/stores/event.ts
+++ client/src/stores/event.ts
@@ -4,113 +4,54 @@ import type { Event } from '../types/event'
 
 const baseURL = 'http://localhost:8081/api/event'
 
+const convertKeysToCamelCase = <T>(obj: { [key: string]: any }): T => {
+  const newObj: { [key: string]: any } = {}
+  for (const key in obj) {
+    // eslint-disable-next-line no-prototype-builtins
+    if (obj.hasOwnProperty(key)) {
+      const snakeCaseKey = key.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase()
+      newObj[snakeCaseKey] = obj[key]
+    }
+  }
+  return newObj as T
+}
+
 export const useEventStore = defineStore('event', {
   state: () => ({
     events: <Event[]>[]
   }),
   actions: {
-    async createEvent(name: string, startDate: string, endDate: string, description: string) {
-      let event = {
-        name: name,
-        startDate: startDate,
-        endDate: endDate,
-        description: description,
-        isPublic: false,
-        isArchived: false
-      }
-
-      const convertKeysToCamelCase = <T>(obj: { [key: string]: any }): T => {
-        const newObj: { [key: string]: any } = {}
-        for (const key in obj) {
-          // eslint-disable-next-line no-prototype-builtins
-          if (obj.hasOwnProperty(key)) {
-            const snakeCaseKey = key.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase()
-            newObj[snakeCaseKey] = obj[key]
-          }
-        }
-        return newObj as T
-      }
-
-      event = await convertKeysToCamelCase(event)
-
+    async createEvent({isPublic = false, isArchived = false, ...partialEvent}: Event) {
       await axios
-        .post(`${baseURL}/create/`, event)
-        .then((res) => {
-          console.log(res)
-        })
-        .catch((error) => {
-          console.log(error)
-        })
+        .post(`${baseURL}/create/`, await convertKeysToCamelCase({isPublic, isArchived, ...partialEvent}))
+        .then(console.log)
+        .catch(console.log)
     },
-    async editEvent(
-      eventId: number,
-      name: string,
-      startDate: string,
-      endDate: string,
-      description: string,
-      isPublic: boolean,
-      isArchived: boolean
-    ) {
-      // change it so it changes the this.event as well
-      let event = {
-        id: eventId,
-        name: name,
-        startDate: startDate,
-        endDate: endDate,
-        description: description,
-        isPublic: isPublic,
-        isArchived: isArchived
-      }
-
-      const convertKeysToCamelCase = <T>(obj: { [key: string]: any }): T => {
-        const newObj: { [key: string]: any } = {}
-        for (const key in obj) {
-          // eslint-disable-next-line no-prototype-builtins
-          if (obj.hasOwnProperty(key)) {
-            const snakeCaseKey = key.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase()
-            newObj[snakeCaseKey] = obj[key]
-          }
-        }
-        return newObj as T
-      }
+    async editEvent(event: Event) {
       event = await convertKeysToCamelCase(event)
 
-      await axios.put(`${baseURL}/update/${event.id}`, event)
+      await axios.put(`${baseURL}/update/${event.eventId}`, event)
+
+      const i = this.events.findIndex(e => e.eventId == event.eventId)
+      this.events[i] = event
     },
     async deleteEvent(eventId: number) {
-      // event must be private
-      
-      // await axios.delete(`${baseURL}/delete/${eventId}`).then(() => {
-      //   console.log('event deleted')
-      // })
-
-      this.events = this.events.filter((event: Event) => event.eventId !== eventId);
-      console.log(this.events);
-      
+      if (!this.events.find(e => eventId == e.eventId)?.isPublic) {
+        await axios.delete(`${baseURL}/delete/${eventId}`)
+        console.log('event deleted')
+      }
     },
     async getEvents() {
       this.events = <Event[]>[]
-      let events = <Event[]>[]
-      await axios.get<Event[]>(`${baseURL}/get/`).then((result) => {
-        events = result.data
-      })
-
-      const convertKeysToCamelCase = <T>(obj: { [key: string]: any }): T => {
-        const newObj: { [key: string]: any } = {}
-        for (const key in obj) {
-          // eslint-disable-next-line no-prototype-builtins
-          if (obj.hasOwnProperty(key)) {
-            const camelCaseKey = key.replace(/_(\w)/g, (_, p1) => p1.toUpperCase())
-            newObj[camelCaseKey] = obj[key]
-          }
-        }
-        return newObj as T
+      let { data } = await axios.get(`${baseURL}/get/`)
+      this.events = await Promise.all(<Event[]>data.map(convertKeysToCamelCase))
+    },
+    async archiveEvent(eventId: number) {
+      const event = this.events.find(e => eventId == e.eventId)
+      if (event) {
+        await this.editEvent({...event, isArchived: true})
+        event.isArchived = true
       }
-
-      for (let i = 0; i < events.length; i++) {
-        events[i] = await convertKeysToCamelCase(events[i])
-      }
-      this.events = [...events]
     }
   },
   getters: {}
