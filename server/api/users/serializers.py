34fa327a598f575589546262ea981ae9d6059b48
Arun Arjunan <arun.arjunan1@outlook.com>
2023-07-05 14:34:32 +0800

    fixed issue with confirm_password field

--- server/api/users/serializers.py
+++ server/api/users/serializers.py
@@ -5,7 +5,7 @@ from .models import User
 class SignUpmodelSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password', 'confirm_password', 'team_signup', 'has_consent', 'travel_method']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password', 'team_signup', 'has_consent', 'travel_method']
         extra_kwargs = {
             'password': {'write_only': True}
         }
@@ -16,7 +16,6 @@ class SignUpmodelSerializer(serializers.ModelSerializer):
             last_name=self.validated_data['last_name'],
             email=self.validated_data['email'],
             password=self.validated_data['password'],
-            confirm_password=self.validated_data['confirm_password'],
             team_signup=self.validated_data['team_signup'],
             has_consent=self.validated_data['has_consent'],
             travel_method=self.validated_data['travel_method'],
