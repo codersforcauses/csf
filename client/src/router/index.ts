faa2870a9e8491484a21eafe7dc4cd53ce93a6ba
ChantelleKerr <chantellekerr93@gmail.com>
2023-07-06 17:23:59 +0800

    fix merge mistake

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,29 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-<<<<<<< HEAD
 import AboutView from '../views/AboutView.vue'
-=======
 import TeamsPageView from '@/views/TeamsPageView.vue'
->>>>>>> main
 import EventsView from '../views/EventsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-<<<<<<< HEAD
       path: '/',
       name: 'about',
       component: AboutView
     },
     {
-      path: '/login',
-      name: 'login',
-      component: LoginView
-=======
       path: '/teams',
       name: 'teams',
       component: TeamsPageView
->>>>>>> main
     },
     {
       path: '/events',
