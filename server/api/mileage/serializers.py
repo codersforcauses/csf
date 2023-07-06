6e50783fab7136c47909c51cf6c58b5454e2179a
Jasper Paterson <jasperpato@gmail.com>
2023-07-06 13:48:30 +0800

    updates

--- server/api/mileage/serializers.py
+++ server/api/mileage/serializers.py
@@ -2,7 +2,7 @@ from .models import Mileage
 from rest_framework.serializers import ModelSerializer
 
 
-class MileageSerialiser(ModelSerializer):
+class MileageSerializer(ModelSerializer):
     class Meta:
         model = Mileage
         fields = '__all__'
--- server/api/mileage/views.py
+++ server/api/mileage/views.py
@@ -1,19 +1,19 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Mileage
-from .serializers import MileageSerialiser
+from .serializers import MileageSerializer
 
 
 @api_view(['GET'])
 def get_mileage(request, user_id):
     event = Mileage.objects.get(user_id=user_id)
-    serializer = MileageSerialiser(event)
+    serializer = MileageSerializer(event)
     print(serializer.data)
     return Response(serializer.data)
 
 @api_view(['POST'])
 def post_mileage(request):
-    serialiser = MileageSerialiser(data=request.data)
-    if serialiser.is_valid():
-        serialiser.save()
-        return Response(serialiser.data)
+    serializer = MileageSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
