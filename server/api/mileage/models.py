00a16a4289f31cb315b5520d2db5fd340454b61f
Jasper Paterson <jasperpato@gmail.com>
2023-07-07 13:37:05 +0800

    km-float-field

--- server/api/mileage/models.py
+++ server/api/mileage/models.py
@@ -6,7 +6,7 @@ from ..users.models import User
 class Mileage(models.Model):
     mileage_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    kilometres = models.IntegerField()
+    kilometres = models.FloatField()
     date = models.DateField(default=datetime.date.today)
 
     def __str__(self):
