5803c1662f1785cfd7b095de3ad79d6b1aadec77
John Smith <makon1234@users.noreply.github.com>
2023-07-07 15:48:36 +0800

    fixed problem with django server

--- server/api/auth/urls.py
+++ server/api/auth/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
-from . import views
-
-app_name = 'auth'
-  
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView
 )
 
+from .views import RegistrationView
+
+app_name = 'auth'
+
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="jwt_token"),
     path("refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
     path("verify/", TokenVerifyView.as_view(), name="jwt_verify"),
-    path("registerUser/", views.registerUser, name="registerUser"),
+    path("register/", RegistrationView.as_view(), name="register"),
 ]
--- server/api/auth/views.py
+++ server/api/auth/views.py
@@ -1,10 +1,11 @@
 from rest_framework import status
 from rest_framework.generics import CreateAPIView
-from rest_fraemwork.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 from .serializers import SignUpmodelSerializer
 
-class registerUser(CreateAPIView):
+class RegistrationView(CreateAPIView):
     serializer_class = SignUpmodelSerializer
 
     def post(self, request):
@@ -12,5 +13,4 @@ class registerUser(CreateAPIView):
         if serializer.is_valid():
             self.perform_create(serializer)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
