9f91fcd819137881371cdfd025104857ef5f9014
Dhruv8601 <dhruvjobanputra8@gmail.com>
2023-07-08 12:06:32 +0800

    pinia store skeleton, removed unused counter store

---
+++ client/src/stores/team.ts
@@ -0,0 +1,10 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+const BASE_URL = 'http://localhost:8081/api'
+
+export const useTeamStore = defineStore('team', {
+  state: () => ({}),
+  getters: {},
+  actions: {}
+})
--- client/src/views/TeamsPageView.vue
+++ client/src/views/TeamsPageView.vue
@@ -8,9 +8,14 @@
 import LoginModal from '../components/LoginModal.vue'
 import CreateOrJoinTeam from '../components/teams/CreateOrJoinTeam.vue'
 import TeamDashboard from '../components/teams/TeamDashboard.vue'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import { useTeamStore } from '../stores/team'
+
+const teamStore = useTeamStore()
 
 // change these values to show the different modals
 const user_id = ref(true)
 const team_id = ref(true)
+
+onMounted(async () => {})
 </script>
