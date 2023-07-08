270c619fcfb952be74273b429e4d25e9029febda
Arun Arjunan <arun.arjunan1@outlook.com>
2023-07-08 13:56:54 +0800

    merge conflict

--- server/api/auth/urls.py
+++ server/api/auth/urls.py
@@ -15,6 +15,5 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="jwt_token"),
     path("refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
     path("verify/", TokenVerifyView.as_view(), name="jwt_verify"),
-    # path("register/", RegistrationView.as_view(), name="register"),
     path("register/", views.register, name="register"),
 ]
