7c1bc23cd3ad5ab84aa9d6b192c7d379d5443a94
K-Straiton <straiton98@gmail.com>
2023-07-09 07:45:54 +0800

    Create view to test modal

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TeamsPageView from '@/views/TeamsPageView.vue'
 import EventsView from '../views/EventsView.vue'
+import MileageModalView from '../views/MileageModalView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/events',
       name: 'events',
       component: EventsView
+    },
+    {
+      path: '/mileagemodal',
+      name: 'mileagemodal',
+      component: MileageModalView
     }
   ]
 })
