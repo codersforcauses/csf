d36ce1c848ec1dea9756b17b44cbb0ff2f401659
Dhruv8601 <dhruvjobanputra8@gmail.com>
2023-07-08 19:57:36 +0800

    added basic structure to functions

--- client/src/stores/team.ts
+++ client/src/stores/team.ts
@@ -1,29 +1,71 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
 import snakify, { type Snakify } from 'snakify-ts'
+import type { Team } from '@/types/team'
+import { useStorage } from '@vueuse/core'
+import camelize from 'camelize-ts'
 
-const BASE_URL = 'http://localhost:8081/api/team'
+const BASE_URL = 'http://localhost:8081/api'
 
 export const useTeamStore = defineStore('team', {
-  state: () => ({}),
+  state: () => ({
+    currentTeam: useStorage('team', null as string | null)
+  }),
+
   getters: {
-    async getTeamId() {}
+    team: (state) => state.currentTeam
   },
+
   actions: {
-    async createTeam(name: String, code: String) {
-      await axios
-        .post(`${BASE_URL}`, {
-          name: name,
-          join_code: code
-        })
-        .then((res) => {
-          if (res.status == 200) {
-            console.log()
-          }
-        })
+    async getTeams() {
+      const teams = await axios.post(`${BASE_URL}`).then((res) => {
+        if (res.status == 200) {
+          // Handle the response
+          return res.data
+        }
+      })
+
+      return camelize(teams)
     },
-    async editTeam() {},
-    async joinTeam() {},
-    async removeTeam() {}
+
+    async getTeam(teamId: Number) {
+      await axios.post(`${BASE_URL}/team/${teamId}/`).then((res) => {
+        if (res.status == 200) {
+          this.currentTeam = JSON.stringify(res.data)
+        }
+      })
+    },
+
+    async createTeam(data: Omit<Team, 'teamId'>) {
+      await axios.post(`${BASE_URL}/team/`, snakify(data)).then((res) => {
+        if (res.status == 200) {
+          this.currentTeam = JSON.stringify(res.data)
+        }
+      })
+    },
+
+    async editTeam(data: Partial<Team>) {
+      await axios.patch(`${BASE_URL}`, snakify(data)).then((res) => {
+        if (res.status == 200) {
+          this.currentTeam = JSON.stringify(res.data)
+        }
+      })
+    },
+
+    async joinTeam(joinCode: String) {
+      await axios.post(`${BASE_URL}/users/${joinCode}`, snakify({ joinCode })).then((res) => {
+        if (res.status == 200) {
+          this.currentTeam = JSON.stringify(res.data)
+        }
+      })
+    },
+
+    async removeTeam(teamId: Number) {
+      await axios.post(`${BASE_URL}/team/${teamId}`, snakify({ teamId })).then((res) => {
+        if (res.status == 200) {
+          this.currentTeam = JSON.stringify(res.data)
+        }
+      })
+    }
   }
 })
