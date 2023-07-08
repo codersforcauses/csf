f2f7c6f5057740609c397c2c25a71da79c072684
Jasper Paterson <jasperpato@gmail.com>
2023-07-08 11:43:04 +0800

    linting

--- server/api/users/migrations/0001_initial.py
+++ server/api/users/migrations/0001_initial.py
@@ -22,9 +22,21 @@ class Migration(migrations.Migration):
             fields=[
                 ('password', models.CharField(max_length=128, verbose_name='password')),
                 ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
-                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
-                ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
-                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+                ('is_superuser', models.BooleanField(
+                    default=False,
+                    help_text='Designates that this user has all permissions without explicitly assigning them.',
+                    verbose_name='superuser status'
+                )),
+                ('is_staff', models.BooleanField(
+                    default=False,
+                    help_text='Designates whether the user can log into this admin site.',
+                    verbose_name='staff status'
+                )),
+                ('is_active', models.BooleanField(
+                    default=True,
+                    help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.',
+                    verbose_name='active'
+                )),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
                 ('id', models.AutoField(primary_key=True, serialize=False)),
                 ('username', models.CharField(max_length=20, unique=True)),
@@ -35,10 +47,32 @@ class Migration(migrations.Migration):
                 ('has_consent', models.BooleanField(default=False)),
                 ('travel_method', models.CharField(blank=True, max_length=100)),
                 ('team_admin', models.BooleanField(default=False)),
-                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
-                ('subteam_id', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='subteam.subteam')),
-                ('team_id', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='team.team')),
-                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
+                ('groups', models.ManyToManyField(
+                    blank=True,
+                    help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+                    related_name='user_set',
+                    related_query_name='user',
+                    to='auth.group',
+                    verbose_name='groups'
+                )),
+                ('subteam_id', models.ForeignKey(
+                    null=True,
+                    on_delete=django.db.models.deletion.CASCADE,
+                    to='subteam.subteam'
+                )),
+                ('team_id', models.ForeignKey(
+                    null=True,
+                    on_delete=django.db.models.deletion.CASCADE,
+                    to='team.team'
+                )),
+                ('user_permissions', models.ManyToManyField(
+                    blank=True,
+                    help_text='Specific permissions for this user.',
+                    related_name='user_set',
+                    related_query_name='user',
+                    to='auth.permission',
+                    verbose_name='user permissions'
+                )),
             ],
             options={
                 'verbose_name': 'user',
