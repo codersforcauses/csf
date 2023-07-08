ea6484e1d7619e6f87698943b639a15040c3e468
K-Straiton <straiton98@gmail.com>
2023-07-08 15:21:22 +0800

    Create basic dialog

---
+++ client/src/components/MileageModal.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-dialog v-model="dialog" :fullscreen="mobile" max-width="912px">
+        <v-card>
+            <div style="height:8px">
+                <v-img src="/images/Footer-min.jpeg" width="100%" cover />
+            </div>
+            <v-card-title>Add Mileage</v-card-title>
+        </v-card>
+
+    </v-dialog>
+</template>
+
+<script setup lang="ts">
+    import { ref } from 'vue'
+    import { useDisplay} from 'vuetify'
+    
+    const { mobile } = useDisplay()
+    const dialog = ref<boolean>(false)
+    const props = defineProps('modelValue')
+    const emits = defineEmits('update:modelValue', 'handleSubmit')
+</script>
+
+<style scoped></style>
