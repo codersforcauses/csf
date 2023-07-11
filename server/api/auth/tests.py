d927454882eed01ba60065b7432d16f427e91eab
Blizzerac <TBlizzerac@hotmail.com>
2023-07-12 00:12:21 +0800

    changed User reference to the custom one

--- server/api/auth/tests.py
+++ server/api/auth/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-from django.contrib.auth.models import User
+from api.users.models import User
 from rest_framework import status
 
 TEST_USERNAME = "afojwieapj;icawe"
