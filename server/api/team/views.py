473d79178189dfd52b2797331b3216bb84e0a6f0
benvarian <88700858+benvarian@users.noreply.github.com>
2023-07-12 19:15:21 +0800

    create & update team

--- server/api/team/views.py
+++ server/api/team/views.py
@@ -6,10 +6,15 @@ from .serializers import TeamSerialiser
 
 @api_view(['POST'])
 def create_team(request):
-    serialiser = TeamSerialiser(data=request.data)
-    if serialiser.is_valid():
-        serialiser.save()
-        return Response(serialiser.data)
+    if (request.user.is_authenticated is False):
+        return Response("User not authenticated", status=401)
+    else:
+        serialiser = TeamSerialiser(data=request.data)
+        if serialiser.is_valid():
+            serialiser.save()
+            return Response(serialiser.data)
+        else:
+            return Response(serialiser.errors, status=400)
 
 
 @api_view(['GET'])
@@ -29,11 +34,19 @@ def get_teams(request):
 
 @api_view(['PUT'])
 def update_team(request, team_id):
-    team = Team.objects.get(team_id=team_id)
-    serializer = TeamSerialiser(instance=team, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
+    if (request.user.is_authenticated is False):
+        return Response("User is not authenticated", status=401)
+    else:
+        if (request.user.team_id != team_id and request.user.team_admin is False):
+            return Response("User is not authorised to edit this team", status=403)
+        else:
+            team = Team.objects.get(team_id=team_id)
+            serializer = TeamSerialiser(instance=team, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors, status=400)
 
 
 @api_view(['DELETE'])
