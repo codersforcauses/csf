ba17897e497d515b74ff33389a4ccfb638d41acd
Jasper Paterson <jasperpato@gmail.com>
2023-07-14 10:21:46 +0800

    get challenge mileages untested

--- server/api/mileage/views.py
+++ server/api/mileage/views.py
@@ -7,10 +7,28 @@ from .serializers import MileageSerializer, UserSerializer #, PostMileageSeriali
 
 import datetime
 
+CHALLENGE_LENGTH = 14 # days
 
 @api_view(['GET'])
 def get_mileage(request, user):
     mileages = Mileage.objects.filter(user=user)
+    
+    if 'challenge' in request.data:
+        user = User.objects.get(id=user)
+
+        if (datetime.date.today() - user.challenge_start_date).days > CHALLENGE_LENGTH:
+            # reset challenge
+            user_data = {
+                'id': request.data['user'],
+                'challenge_start_date': user.challenge_start_date or datetime.date.today()
+            }
+            user_serializer = UserSerializer(instance=user, data=user_data)
+            if user_serializer.is_valid():
+                user_serializer.save()
+
+        # only get mileages within current challenge period
+        mileages = filter(lambda m: m.date >= user_serializer.challenge_start_date, mileages)
+
     serializer = MileageSerializer(mileages, many=True)
     return Response(serializer.data)
 
@@ -26,11 +44,9 @@ def post_mileage(request):
     user_serializer = UserSerializer(instance=user, data=user_data)
     if user_serializer.is_valid():
         user_serializer.save()
-    
-    serializer = MileageSerializer(data=request.data)
-    if serializer.is_valid():
-        mileage = serializer.save()
-        response_data = MileageSerializer(mileage).data
-        return Response(response_data, status=status.HTTP_201_CREATED)
-
+        serializer = MileageSerializer(data=request.data)
+        if serializer.is_valid():
+            mileage = serializer.save()
+            response_data = MileageSerializer(mileage).data
+            return Response(response_data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
