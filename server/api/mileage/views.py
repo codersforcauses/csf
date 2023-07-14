bb40feb3ca29e889a6d798619bb384605ab2a6e8
Jasper Paterson <jasperpato@gmail.com>
2023-07-14 13:36:58 +0800

    challenge-start-date

--- server/api/mileage/views.py
+++ server/api/mileage/views.py
@@ -9,25 +9,17 @@ import datetime
 
 CHALLENGE_LENGTH = 14 # days
 
+
 @api_view(['GET'])
 def get_mileage(request, user):
     mileages = Mileage.objects.filter(user=user)
     
-    if 'challenge' in request.data:
+    if 'challenge' in request.data: # only get mileages within current challenge period
         user = User.objects.get(id=user)
-
-        if (datetime.date.today() - user.challenge_start_date).days > CHALLENGE_LENGTH:
-            # reset challenge
-            user_data = {
-                'id': request.data['user'],
-                'challenge_start_date': user.challenge_start_date or datetime.date.today()
-            }
-            user_serializer = UserSerializer(instance=user, data=user_data)
-            if user_serializer.is_valid():
-                user_serializer.save()
-
-        # only get mileages within current challenge period
-        mileages = filter(lambda m: m.date >= user_serializer.challenge_start_date, mileages)
+        mileages = filter(
+            lambda m: user.challenge_start_date and m.date >= user.challenge_start_date,
+            mileages
+        )
 
     serializer = MileageSerializer(mileages, many=True)
     return Response(serializer.data)
@@ -35,18 +27,28 @@ def get_mileage(request, user):
 
 @api_view(['POST'])
 def post_mileage(request):
-    # start challenge for User if not already started
     user = User.objects.get(id=request.data['user'])
+
+    # start challenge for User if not already started
+    challenge_start_date = user.challenge_start_date or datetime.date.today()
+
+    # reset challenge if time is up
+    if (datetime.date.today() - user.challenge_start_date).days > CHALLENGE_LENGTH:
+        challenge_start_date = datetime.date.today()
+
     user_data = {
         'id': request.data['user'],
-        'challenge_start_date': user.challenge_start_date or datetime.date.today()
+        'challenge_start_date': challenge_start_date
     }
+
     user_serializer = UserSerializer(instance=user, data=user_data)
     if user_serializer.is_valid():
         user_serializer.save()
+        
         serializer = MileageSerializer(data=request.data)
         if serializer.is_valid():
             mileage = serializer.save()
             response_data = MileageSerializer(mileage).data
             return Response(response_data, status=status.HTTP_201_CREATED)
+        
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
