249d4935eff48fd597ef4fd75e0fc9c880fbb01e
Amy Neve <ladyames20203@gmail.com>
2023-07-14 15:48:32 +0800

    basic styling

--- client/src/views/NotFoundView.vue
+++ client/src/views/NotFoundView.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="ma-0 pl-4 pr-4 pt-4 pb-0" align="center" justify="center">
-    <h1 class="text-md-h1 mb-md-10 mb-7 font-weight-medium" id="title">404</h1>
-    <h2 class="text-md-h2 mb-md-10 mb-7 font-weight-medium">Page Not Found</h2>
-    <p class="pb-5">The page you're looking for does not exist</p>
+  <div class="ma-0 px-4 pt-4 pb-0 text-center flex align-center justify-center">
+    <h1 class="mb-md-3 font-weight-medium" id="title">404</h1>
+    <h2 class="text-h4 text-md-h3 mb-md-5 mb-3 font-weight-medium">Page Not Found</h2>
+    <p class="pb-5 pb-md-7">The page you're looking for does not exist.</p>
     <v-btn
       href="/"
       target="_blank"
-      class="bg-primaryRed pb-14 mb-8"
-      size="x-large"
-      variant="flat"
+      class="text-primaryWhite bg-primaryRed mr-3 pb-1"
+      size="large"
+      :style="{ fontFamily: 'Hackney', fontSize: '28px' }"
       style="letter-spacing: 0.5px"
     >
       Return Home
@@ -21,6 +21,7 @@
 <style scoped>
 #title {
   font-family: 'Hackney' !important;
-  line-height: 1.2em;
+  font-size: 7em;
+  color: black;
 }
 </style>
