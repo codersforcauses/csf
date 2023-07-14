7c473fd97c64cfbba5ce8dd1184cd57753e38e41
Jasper Paterson <jasperpato@gmail.com>
2023-07-14 09:12:42 +0800

    challenge-start-date

--- server/api/mileage/serializers.py
+++ server/api/mileage/serializers.py
@@ -1,8 +1,17 @@
 from .models import Mileage
+from ..users.models import User
 from rest_framework.serializers import ModelSerializer
 
 
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['challenge_start_date']
+
 class MileageSerializer(ModelSerializer):
+
+    # user = UserSerializer()
+
     class Meta:
         model = Mileage
         fields = '__all__'
