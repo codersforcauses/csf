6921351ab0b1dc0eeb4f91187a0bb519e7488ca6
Alex Hawking <alexhawking23@gmail.com>
2023-07-15 12:16:17 +0800

    edit store

--- client/src/stores/openModal.ts
+++ client/src/stores/openModal.ts
@@ -4,8 +4,6 @@ export const useModalStateStore = defineStore('modalState', {
   state: () => {
     return { modalState: false }
   },
-  // could also be defined as
-  // state: () => ({ count: 0 })
   actions: {
     switchState() {
       this.modalState = !this.modalState
