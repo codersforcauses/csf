fbc6a933ae7377820bb678e7fb3bd05644b0e140
Adib Rohani <adibrohan54@gmail.com>
2023-07-15 15:16:18 +0800

    added search field

--- client/src/views/LeaderboardView.vue
+++ client/src/views/LeaderboardView.vue
@@ -1,26 +1,40 @@
 <template>
-    <div class="header">
-        <h1 class="text-center text-secondaryTint">Leaderboards</h1>
-    </div>
-    <v-row class="ma-0">
-        <v-chip
-            :color="activeButton === board ? 'secondaryBlue' : ''"
-            v-for="board in ['Individual', 'Teams']"
-            :key="board"
-            rounded="xl"
-            @click="activeButton = board">{{ board }}</v-chip>
-    </v-row>
+  <div class="header">
+    <h1 class="text-center text-secondaryTint">Leaderboards</h1>
+  </div>
+  <v-row class="ma-0 justify-center">
+    <v-chip
+      :color="activeButton === board ? 'secondaryBlue' : ''"
+      v-for="board in ['Individual', 'Teams']"
+      :key="board"
+      rounded="xl"
+      @click="activeButton = board"
+      >{{ board }}</v-chip
+    >
+  </v-row>
+  <v-row class="ma-0 pa-4">
+    <v-text-field
+      prepend-inner-icon="mdi-magnify"
+      hide-details
+      variant="outlined"
+      placeholder="Search Leaderboard"
+      class="mx-3"
+      clearable
+      v-model="searchQuery"
+    />
+  </v-row>
 </template>
 
 <script setup lang="ts">
-    import { ref } from 'vue'
+import { ref } from 'vue'
 
-    const activeButton = ref('Individual')
+const activeButton = ref('Individual')
+const searchQuery = ref('')
 </script>
 
 <style>
-    .header {
-        background-image: url('/images/Footer-min.jpeg');
-        background-size: cover;
-    }
+.header {
+  background-image: url('/images/Footer-min.jpeg');
+  background-size: cover;
+}
 </style>
