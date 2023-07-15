325517f1a21342bd1f3dc3b351182cb8cc47817b
Adib Rohani <adibrohan54@gmail.com>
2023-07-15 13:05:51 +0800

    changing how dates are displayed and other configs

--- client/src/components/MileageGraph.vue
+++ client/src/components/MileageGraph.vue
@@ -4,13 +4,13 @@
       <v-btn
         class="text-secondaryGreen"
         variant="tonal"
-        v-for="duration in ['This Week', 'This Month', 'This Year', 'Overall']"
-        :key="duration"
+        v-for="range in ['This Week', 'This Month', 'This Year', 'Overall']"
+        :key="range"
         rounded="xl"
         size="small"
-        @click="filterData(duration)"
+        @click="filterData(range)"
       >
-        {{ duration }}
+        {{ range }}
       </v-btn>
     </v-card-actions>
     <LineWithLineChart :data="graphData" :options="options" />
@@ -35,6 +35,7 @@ import type Mileage from '../types/mileage'
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend) // creating instance of chart
 
+// DATA FILTERING AND COMPUTED PROPERTIES
 const tempData = ref<Mileage[]>([
   { mileage_id: 1, user_id: 1, kilometers: 10, date: '2023-06-01' },
   { mileage_id: 2, user_id: 1, kilometers: 20, date: '2023-06-02' },
@@ -77,16 +78,19 @@ const tempData = ref<Mileage[]>([
 ])
 
 const filteredData = ref<Mileage[]>()
+const labels = computed(() => filteredData.value?.map((data) => data.date))
+const data = computed(() => filteredData.value?.map((data) => data.kilometers))
 
-function filterData(duration: string) {
+function filterData(range: string) {
   let minDate
   const currentDate = new Date()
 
-  if (duration === 'This Week') {
-    minDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - 7)
-  } else if (duration === 'This Month') {
+  //filtering the data based on the range selected
+  if (range === 'This Week') {
+    minDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - 6)
+  } else if (range === 'This Month') {
     minDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, currentDate.getDate())
-  } else if (duration === 'This Year') {
+  } else if (range === 'This Year') {
     minDate = new Date(currentDate.getFullYear() - 1, currentDate.getMonth(), currentDate.getDate())
   } else {
     minDate = tempData.value.reduce((min, obj) => {
@@ -98,50 +102,97 @@ function filterData(duration: string) {
   filteredData.value = []
   while (currentDate >= minDate) {
     const dateString = currentDate.toISOString().split('T')[0]
-    const matchingData = tempData.value.find((obj) => obj.date === dateString)
+    // changing labels based on the range selected
+    let datalabel = ''
 
+    const daysOfWeek = ['Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri']
+    const monthsOfYear = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sept',
+      'Oct',
+      'Nov',
+      'Dec'
+    ]
+
+    const day = currentDate.getDate()
+    const weekday = daysOfWeek[currentDate.getDay()]
+    const month = monthsOfYear[currentDate.getMonth()]
+    const year = currentDate.getFullYear()
+
+    if (range === 'This Week') {
+      datalabel = `${weekday} ${day}`
+    }
+    if (range === 'This Year' || range === 'This Month') {
+      datalabel = `${month} ${day}`
+    }
+    if (range === 'Overall') {
+      datalabel = `${month} ${day}, ${year}`
+    }
+
+    //making sure dates that don't exist in the database are still included as 0 km
+    const matchingData = tempData.value.find((obj) => obj.date === dateString)
     filteredData.value?.unshift({
       mileage_id: matchingData ? matchingData.mileage_id : -1,
       user_id: matchingData ? matchingData.user_id : -1,
       kilometers: matchingData ? matchingData.kilometers : 0,
-      date: dateString
+      date: datalabel ? datalabel : dateString
     })
-    console.log(filteredData.value)
     currentDate.setDate(currentDate.getDate() - 1)
   }
 }
 
 filterData('This Week')
 
-const labels = computed(() => filteredData.value?.map((data) => data.date))
-const data = computed(() => filteredData.value?.map((data) => data.kilometers))
-
+// GRAPH DATA
 const graphData = computed(() => {
-  console.log(labels.value, data.value)
   return {
+    type: 'line',
     labels: labels.value,
     datasets: [
       {
-        data: data.value,
-        borderColor: 'rgb(255, 99, 99)',
-        fill: false,
-        cubicInterpolationMode: 'monotone',
-        tension: 0.4
+        data: data.value
       } as ChartDataset<'line', (number | null)[]> // Update the type assertion
     ]
   }
 })
 
+// GRAPH CONFIGURATION
 const options = {
+  borderColor: 'rgb(255, 99, 99)',
+  fill: false,
+  cubicInterpolationMode: 'monotone',
+  lineTension: 0.8,
+  responsive: true,
+  maintainAspectRatio: true,
   plugins: {
     legend: {
       display: false
     }
   },
-  responsive: true,
-  maintainAspectRatio: true,
-  tooltips: {
-    intersect: false
+  scales: {
+    x: {
+      grid: {
+        display: false
+      },
+      title: {
+        display: true,
+        text: 'Date'
+      }
+    },
+    y: {
+      beginAtZero: true,
+      title: {
+        display: true,
+        text: 'Distance (km)'
+      }
+    }
   }
 }
 </script>
