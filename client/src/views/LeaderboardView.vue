e9d3bc728902bfc6a079538b9284c6fcd4e4cfd8
K-Straiton <straiton98@gmail.com>
2023-07-15 14:49:07 +0800

    Add buttons

--- client/src/views/LeaderboardView.vue
+++ client/src/views/LeaderboardView.vue
@@ -1,9 +1,26 @@
 <template>
-    
+    <div class="header">
+        <h1 class="text-center text-secondaryTint">Leaderboards</h1>
+    </div>
+    <v-row class="ma-0">
+        <v-chip
+            :color="activeButton === board ? 'secondaryBlue' : ''"
+            v-for="board in ['Individual', 'Teams']"
+            :key="board"
+            rounded="xl"
+            @click="activeButton = board">{{ board }}</v-chip>
+    </v-row>
 </template>
 
 <script setup lang="ts">
+    import { ref } from 'vue'
 
+    const activeButton = ref('Individual')
 </script>
 
-<style></style>
+<style>
+    .header {
+        background-image: url('/images/Footer-min.jpeg');
+        background-size: cover;
+    }
+</style>
