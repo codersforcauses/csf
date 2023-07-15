eeb30cbcbe9ab166a676e4ade05a4f8945218d9c
YunnTun <22676427@student.uwa.edu.au>
2023-07-15 12:25:57 +0800

    debug registerSW.js

--- client/dev-dist/registerSW.js
+++ client/dev-dist/registerSW.js
@@ -1,1 +1,4 @@
-if('serviceWorker' in navigator) navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' });
+    console.log("SW in navigator!")
+}
