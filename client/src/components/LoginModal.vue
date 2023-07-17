codersforcauses/csf

commit 9dc75025e00dcf25a01dab44c2c2fac48ca50c79

implemented #154
Co-authored-by: ChantelleKerr <chantellekerr93@gmail.com>

client/src/components/LoginModal.vue
@@ -1,5 +1,11 @@
 <template>
-  <v-dialog v-model="dialog" :fullscreen="isFullscreen" max-width="500px" max-height="100vh">
+  <v-dialog
+    v-model="dialog"
+    :fullscreen="isFullscreen"
+    max-width="500px"
+    max-height="100vh"
+    v-if="modalStore.isLogin"
+  >
     <v-card class="bg-backgroundGrey" align="center" justify="center">
       <div class="bg-backgroundGrey">
         <v-container class="pa-0">
@@ -14,7 +20,7 @@
           </v-col>
           <v-spacer />
           <v-col cols="auto">
-            <v-btn variant="plain" @click="closeModal">
+            <v-btn variant="plain" @click="modalStore.close">
               <v-icon icon="mdi-close" size="32px" />
             </v-btn>
           </v-col>
@@ -74,7 +80,12 @@
             <v-row align="center" justify="center" class="mt-4">
               <p>
                 or
-                <v-btn variant="plain" color="secondaryBlue" style="font-size: 12px">
+                <v-btn
+                  variant="plain"
+                  color="secondaryBlue"
+                  style="font-size: 12px"
+                  @click="modalStore.register"
+                >
                   Register
                 </v-btn>
               </p>
@@ -195,16 +206,17 @@
 import { ref, watchEffect } from 'vue'
 import { useUserStore } from '@/stores/user'
 import { AxiosError } from 'axios'
+import { useModalStore } from '@/stores/modal'
 
 const dialog = ref(true)
 
 const userStore = useUserStore()
+const modalStore = useModalStore()
 const page = ref<1 | 2 | 3 | 4 | 5>(1)
 const isFullscreen = ref(false)
 
 const form = ref({
-  username: '',
-  password: '',
+  ...modalStore.computedFields,
   email: '',
   token: '',
   newPassword: '',
@@ -218,23 +230,13 @@ const errors = ref({
   newPassword: ''
 })
 
-defineProps(['loginModal'])
-const emit = defineEmits(['openLoginModal'])
-
 const required = (v: string) => !!v || 'Field is required'
 
-function isEmail(candidate: string) {
-  return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(candidate)
-}
-
-const closeModal = () => {
-  emit('openLoginModal', false)
-}
+const isEmail = (candidate: string) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(candidate)
 
 const submitForm = async () => {
-  if (await userStore.loginUser(form.value.username, form.value.password)) {
-    closeModal()
-  } else {
+  if (await userStore.loginUser(form.value.username, form.value.password)) modalStore.close()
+  else {
     userStore.authToken = null
     userStore.authUser = null
     errors.value.login = 'Your username or password is incorrect'
@@ -299,3 +301,4 @@ watchEffect(async () => {
   }
 })
 </script>
+@/stores/modal

client/src/components/SignUpModal.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-dialog v-model="dialog" :fullscreen="isFullscreen" max-width="500px">
+  <v-dialog
+    v-model="dialog"
+    :fullscreen="isFullscreen"
+    max-width="500px"
+    v-if="modalStore.isRegister"
+  >
     <v-card style="height: 100%">
       <div v-if="firstPage">
         <form class="bg-backgroundGrey">
@@ -8,7 +13,7 @@
           </v-container>
           <v-row justify="end">
             <v-col cols="auto">
-              <v-btn variant="text" @click="closeModal">
+              <v-btn variant="text" @click="modalStore.close">
                 <v-icon icon="mdi-close" size="32px"></v-icon
               ></v-btn>
             </v-col>
@@ -112,7 +117,11 @@
               <v-row align="center" justify="center">
                 <v-col cols="auto">
                   <!-- todo add in routing to login modal when its ready -->
-                  <v-btn variant="text" color="secondaryBlue" style="font-size: 12px"
+                  <v-btn
+                    variant="text"
+                    color="secondaryBlue"
+                    style="font-size: 12px"
+                    @click="modalStore.login"
                     >Already Have an account?</v-btn
                   >
                 </v-col>
@@ -128,7 +137,7 @@
           </v-container>
           <v-row justify="end">
             <v-col cols="auto">
-              <v-btn variant="text" @click="closeModal">
+              <v-btn variant="text" @click="modalStore.close">
                 <v-icon icon="mdi-close" size="32px"></v-icon
               ></v-btn>
             </v-col>
@@ -253,14 +262,10 @@ import { useUserStore } from '../stores/user'
 import snakify from 'snakify-ts'
 import { AxiosError } from 'axios'
 import camelize from 'camelize-ts'
+import { useModalStore } from '@/stores/modal'
 const userStore = useUserStore()
+const modalStore = useModalStore()
 
-defineProps(['dialogModal'])
-const emit = defineEmits(['openSignUpModal'])
-
-const closeModal = () => {
-  emit('openSignUpModal', false)
-}
 const isFullscreen = ref(false)
 const firstPage = ref<boolean>(true)
 const dialog = ref(true)
@@ -278,18 +283,17 @@ const travelMethod = ref([
   { logo: 'mdi-walk', mode: 'WALKING', isSelected: false }
 ])
 
-const state = reactive<Signup>({
-  username: '',
+const state = reactive({
+  ...modalStore.computedFields,
   firstName: '',
   lastName: '',
   email: '',
-  password: '',
   confirmPassword: '',
   teamSignup: false,
   hasConsent: false,
   avatar: '',
   travelMethod: ''
-})
+}) as Signup
 
 const errorMsg = ref('')
 const initialErrors = {
@@ -382,3 +386,4 @@ watchEffect(async () => {
   background-color: #345e9e !important;
 }
 </style>
+@/stores/modal

client/src/stores/modal.ts
@@ -1,0 +1,37 @@
+import { defineStore } from 'pinia'
+import { computed } from 'vue'
+
+export const useModalStore = defineStore('modalState', {
+  state: () => ({
+    _status: 'closed' as 'closed' | 'register' | 'login',
+    fields: { username: '', password: '' }
+  }),
+  getters: {
+    isRegister: (state) => state._status === 'register',
+    isLogin: (state) => state._status === 'login',
+    isOpen: (state) => state._status !== 'closed',
+    computedFields: (state) => ({
+      username: computed({
+        get: () => state.fields.username,
+        set: (v) => (state.fields.username = v)
+      }),
+      password: computed({
+        get: () => state.fields.password,
+        set: (v) => (state.fields.password = v)
+      })
+    })
+  },
+  actions: {
+    register() {
+      this._status = 'register'
+    },
+    login() {
+      this._status = 'login'
+    },
+    close() {
+      this._status = 'closed'
+      this.fields.username = ''
+      this.fields.password = ''
+    }
+  }
+})

client/src/components/NavbarComponents.vue
@@ -53,7 +53,7 @@
           variant="flat"
           :style="{ fontFamily: 'Hackney', fontSize: '20px' }"
           style="letter-spacing: 0.5px"
-          @click="openLoginModal"
+          @click="modalStore.login"
           >LOGIN</v-btn
         >
         <v-btn
@@ -63,7 +63,7 @@
           variant="flat"
           :style="{ fontFamily: 'Hackney', fontSize: '20px' }"
           style="letter-spacing: 0.5px"
-          @click="openSignUpModal"
+          @click="modalStore.register"
           >SIGNUP</v-btn
         >
       </v-row>
@@ -166,7 +166,7 @@
             variant="flat"
             :style="{ fontFamily: 'Hackney', fontSize: '28px' }"
             style="letter-spacing: 0.5px"
-            @click="openLoginModal"
+            @click="modalStore.login"
             >LOGIN</v-btn
           >
           <v-btn
@@ -176,7 +176,7 @@
             variant="flat"
             :style="{ fontFamily: 'Hackney', fontSize: '28px' }"
             style="letter-spacing: 0.5px"
-            @click="openSignUpModal"
+            @click="modalStore.register"
             >SIGNUP</v-btn
           >
         </v-row>
@@ -186,17 +186,9 @@
 
   <v-img :src="FooterBanner" width="100%" height="8" class="sticky-nav-img" cover />
 
-  <SignUpModal
-    :dialog-modal="signupModal"
-    v-if="signupModal"
-    @open-signUp-modal="openSignUpModal"
-  />
+  <SignUpModal />
 
-  <LoginModal
-    :dialog-modal="ModalStateStore.getState"
-    v-if="ModalStateStore.getState"
-    @open-login-modal="openLoginModal"
-  />
+  <LoginModal />
   <PopupDialog
     v-model="openConfirmLogoutDialogueBox"
     title="Confirm Logout"
@@ -218,30 +210,20 @@ import { useUserStore } from '@/stores/user'
 import { storeToRefs } from 'pinia'
 
 import { useRouter } from 'vue-router'
-
-import { useModalStateStore } from '@/stores/openModal'
-const ModalStateStore = useModalStateStore()
+import { useModalStore } from '@/stores/modal'
 
 const router = useRouter()
 
 const userStore = useUserStore()
+const modalStore = useModalStore()
 
 const { user } = storeToRefs(userStore)
 
 const { mobile } = useDisplay()
 const dialog = ref<boolean>(false)
-const signupModal = ref<boolean>(false)
 const openConfirmLogoutDialogueBox = ref<boolean>(false)
 
 let homelink = '/'
-
-const openSignUpModal = () => {
-  signupModal.value = !signupModal.value
-}
-
-const openLoginModal = () => {
-  ModalStateStore.switchState()
-}
 
 const openPopupDialog = () => {
   openConfirmLogoutDialogueBox.value = true
@@ -306,3 +288,4 @@ const menu = [
   background-color: #ed1c24;
 }
 </style>
+@/stores/modal

client/src/router/index.ts
@@ -7,7 +7,7 @@ import UserSettingsView from '../views/UserSettingsView.vue'
 import DashboardView from '../views/DashboardView.vue'
 import ChallengeView from '../views/ChallengeView.vue'
 import { capitalize } from 'vue'
-import { useModalStateStore } from '@/stores/openModal'
+import { useModalStore } from '@/stores/modal'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -47,11 +47,11 @@ const router = createRouter({
 
 router.beforeEach(async (to, from) => {
   const userStore = useUserStore()
-  const modalStateStore = useModalStateStore()
+  const modalStore = useModalStore()
 
   if (to.path == '/team' || to.path == '/dashboard') {
     if (userStore.user == null) {
-      modalStateStore.switchState()
+      modalStore.login()
       // Cancel navigation if not logged in
       return false
     }

client/src/stores/openModal.ts
@@ -1,13 +1,0 @@
-import { defineStore } from 'pinia'
-
-export const useModalStateStore = defineStore('modalState', {
-  state: () => ({ modalState: false }),
-  getters: {
-    getState: (state: { modalState: boolean }) => state.modalState
-  },
-  actions: {
-    switchState() {
-      this.modalState = !this.modalState
-    }
-  }
-})

commit b2e8125b61e6160fad38b3bb024a496f432db326

fix issues caused by #160 + general improvements

client/src/components/LoginModal.vue
@@ -1,10 +1,9 @@
 <template>
   <v-dialog
-    v-model="dialog"
+    v-model="modalStore.isLogin"
     :fullscreen="isFullscreen"
     max-width="500px"
     max-height="100vh"
-    v-if="modalStore.isLogin"
   >
     <v-card class="bg-backgroundGrey" align="center" justify="center">
       <div class="bg-backgroundGrey">
@@ -44,7 +43,7 @@
             :rules="[required]"
             :error-messages="errors.login"
             @focus="errors.login = ''"
-            v-model="form.username"
+            v-model="modalStore.username"
             label="Username"
             required
           />
@@ -52,7 +51,7 @@
             bg-color="#FFFFFF"
             :rules="[required]"
             @focus="errors.login = ''"
-            v-model="form.password"
+            v-model="modalStore.password"
             label="Password"
             type="password"
             required
@@ -208,7 +207,6 @@ import { useUserStore } from '@/stores/user'
 import { AxiosError } from 'axios'
 import { useModalStore } from '@/stores/modal'
 
-const dialog = ref(true)
 
 const userStore = useUserStore()
 const modalStore = useModalStore()
@@ -216,7 +214,6 @@ const page = ref<1 | 2 | 3 | 4 | 5>(1)
 const isFullscreen = ref(false)
 
 const form = ref({
-  ...modalStore.computedFields,
   email: '',
   token: '',
   newPassword: '',
@@ -235,7 +232,7 @@ const required = (v: string) => !!v || 'Field is required'
 const isEmail = (candidate: string) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(candidate)
 
 const submitForm = async () => {
-  if (await userStore.loginUser(form.value.username, form.value.password)) modalStore.close()
+  if (await userStore.loginUser(modalStore.username, modalStore.password)) modalStore.close()
   else {
     userStore.authToken = null
     userStore.authUser = null

client/src/components/SignUpModal.vue
@@ -1,9 +1,8 @@
 <template>
   <v-dialog
-    v-model="dialog"
+    v-model="modalStore.isRegister"
     :fullscreen="isFullscreen"
     max-width="500px"
-    v-if="modalStore.isRegister"
   >
     <v-card style="height: 100%">
       <div v-if="firstPage">
@@ -263,12 +262,12 @@ import snakify from 'snakify-ts'
 import { AxiosError } from 'axios'
 import camelize from 'camelize-ts'
 import { useModalStore } from '@/stores/modal'
+import { storeToRefs } from 'pinia'
 const userStore = useUserStore()
 const modalStore = useModalStore()
 
 const isFullscreen = ref(false)
 const firstPage = ref<boolean>(true)
-const dialog = ref(true)
 const avatarPaths = ref([
   { url: 'avatar1.jpg', alt: 'avatar1', isSelected: true },
   { url: 'avatar2.jpg', alt: 'avatar2', isSelected: false },
@@ -283,8 +282,10 @@ const travelMethod = ref([
   { logo: 'mdi-walk', mode: 'WALKING', isSelected: false }
 ])
 
+const {username, password} = storeToRefs(modalStore)
 const state = reactive({
-  ...modalStore.computedFields,
+  username,
+  password,
   firstName: '',
   lastName: '',
   email: '',

client/src/stores/modal.ts
@@ -1,37 +1,48 @@
 import { defineStore } from 'pinia'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
-export const useModalStore = defineStore('modalState', {
-  state: () => ({
-    _status: 'closed' as 'closed' | 'register' | 'login',
-    fields: { username: '', password: '' }
-  }),
-  getters: {
-    isRegister: (state) => state._status === 'register',
-    isLogin: (state) => state._status === 'login',
-    isOpen: (state) => state._status !== 'closed',
-    computedFields: (state) => ({
-      username: computed({
-        get: () => state.fields.username,
-        set: (v) => (state.fields.username = v)
-      }),
-      password: computed({
-        get: () => state.fields.password,
-        set: (v) => (state.fields.password = v)
-      })
-    })
-  },
-  actions: {
-    register() {
-      this._status = 'register'
-    },
-    login() {
-      this._status = 'login'
-    },
-    close() {
-      this._status = 'closed'
-      this.fields.username = ''
-      this.fields.password = ''
-    }
+
+// export const useModalStore = defineStore('modalState', {
+//   state: () => ({
+//     status: ,
+//     username: '',
+//     password: '',
+//   }),
+//   actions: {
+//     register() {
+//       this.status = 'register'
+//     },
+//     login() {
+//       this.status = 'login'
+//     },
+//     close() {
+//       this.status = 'closed'
+//     }
+//   }
+// })
+
+export const useModalStore = defineStore('modalState', () => {
+  const status = ref<'closed' | 'login' | 'register'>('closed')
+  const username = ref('')
+  const password = ref('')
+  
+  const register = () => status.value = 'register'
+  const login = () => status.value = 'login'
+  const close = () => {
+    status.value = 'closed'
+    username.value = ''
+    password.value = ''
   }
+
+  const isRegister = computed({
+    get: () => status.value === 'register',
+    set: v => v ? register() : close()
+  })
+  const isLogin = computed({
+    get: () => status.value === 'login',
+    set: v => v ? login() : close()
+  })
+  const isOpen = computed(() => status.value !== 'closed')
+
+  return {username, password, register, login, close, isRegister, isLogin, isOpen}
 })

client/src/views/AboutView.vue
@@ -38,7 +38,7 @@
       variant="flat"
       :style="{ fontFamily: 'Hackney', fontSize: '42px' }"
       style="letter-spacing: -1px"
-      @click="openSignUpModal"
+      @click="modalStore.register"
     >
       REGISTER NOW
     </v-btn>
@@ -111,27 +111,20 @@
     </p></v-row
   >
   <Footer />
-  <SignUpModal
-    :dialog-modal="signupModal"
-    v-if="signupModal"
-    @open-signUp-modal="openSignUpModal"
-  />
 </template>
 
 <script setup lang="ts">
+
+
 import Footer from '@/components/Footer.vue'
-import { ref } from 'vue'
-import SignUpModal from '@/components/SignUpModal.vue'
 import studentsLearning from '/images/students_learning.webp'
 import thankYou from '/images/thank_you.jpg'
 import StrideForEducationGif from '/gif/Stride_1.gif'
 import CSFBanner from '/images/CSF_Banner.jpg'
 import StrideForEducationRBGLogo from '/images/SFE_RGB_Logo.png'
 
-const signupModal = ref<boolean>(false)
-const openSignUpModal = () => {
-  signupModal.value = !signupModal.value
-}
+import { useModalStore } from '@/stores/modal'
+const modalStore = useModalStore()
 
 const beliefs = [
   'Every young person we work with should be able to dream big and be proud.',