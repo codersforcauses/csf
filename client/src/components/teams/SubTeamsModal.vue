08e486cdf0344b173fd81fe81c04b71d670cf1b6
Nisarg Dave <ndave630@gmail.com>
2023-07-18 21:09:08 +0800

    Created new memberId variable and removed console logs

--- client/src/components/teams/SubTeamsModal.vue
+++ client/src/components/teams/SubTeamsModal.vue
@@ -1,117 +1,120 @@
 <script setup lang="ts">
-import { ref, reactive, toRef, computed, watch} from 'vue'
-import type { Ref, PropType} from 'vue';
+import { ref, reactive, toRef, computed, watch } from 'vue'
+import type { Ref, PropType } from 'vue'
 
 import ConfirmButton from '@/components/ConfirmButton.vue'
 import PopupDialog from '@/components/PopupDialog.vue'
 
-import type {SubteamView, MemberView} from '@/types/subteam'
+import type { SubteamView, MemberView } from '@/types/subteam'
 
 // Open dialog, get methods from SubTeams.vue
 const emit = defineEmits(['saveTeam', 'removeSubTeam', 'update:modelValue'])
 
 const props = defineProps({
   selectedSubteam: {
-      type: Object as PropType<SubteamView>,
-      required: true
+    type: Object as PropType<SubteamView>,
+    required: true
   },
   modelValue: {
     type: Boolean,
     required: true
   },
   avaliableMemeberList: {
-        type: Array as PropType<MemberView[]>,
-        required: true
+    type: Array as PropType<MemberView[]>,
+    required: true
   }
 })
 
-let selectedSubteamCopy : SubteamView= reactive({
-    teamName: '',
-    teamId: '',
-    subteamId: '',
-    totalKM: '',
-    members: [
-        {
-            id: 13,
-            firstname: 'Unknown',
-            lastname: 'Unknown',
-            avatar: 'https://cdn.vuetifyjs.com/images/john.png'
-        }
-    ]
+let selectedSubteamCopy: SubteamView = reactive({
+  teamName: '',
+  teamId: '',
+  subteamId: '',
+  totalKM: '',
+  members: [
+    {
+      id: 13,
+      firstname: 'Unknown',
+      lastname: 'Unknown',
+      avatar: 'https://cdn.vuetifyjs.com/images/john.png'
+    }
+  ]
 })
 
 //Modify selectedSubteam Copy
-watch(() => props.selectedSubteam, (first) => {
+watch(
+  () => props.selectedSubteam,
+  (first) => {
     //local selectedTeam Copy to change values
     selectedSubteamCopy = reactive({
-        teamName: first.teamName,
-        teamId: first.teamId,
-        subteamId: first.subteamId,
-        totalKM: first.totalKM,
-        members: first.members
+      teamName: first.teamName,
+      teamId: first.teamId,
+      subteamId: first.subteamId,
+      totalKM: first.totalKM,
+      members: first.members
     })
-});
+  }
+)
 
 // edit sub team name input rules
 const rules = {
-    required: (value: string) => !!value || 'Field is required'
+  required: (value: string) => !!value || 'Field is required'
 }
 
 //avaliable member list
 const avaliableMemeberList: Ref<MemberView[]> = toRef(props, 'avaliableMemeberList')
 const selectedMember: Ref<MemberView> = ref({
-    id: 0,
-    firstname: '',
-    lastname: '',
-    avatar: ''
+  id: 0,
+  firstname: '',
+  lastname: '',
+  avatar: ''
 })
 
 const addMember = () => {
-    if (selectedMember.value !== null) {
-      selectedSubteamCopy.members.push(selectedMember.value)
-        // Removing that member from the avaliable members drop down list
-        for (let i = 0; i < avaliableMemeberList.value.length; i++) {
-            if (avaliableMemeberList.value[i].id === selectedMember.value.id) {
-              avaliableMemeberList.value.splice(i, 1)
-            }
-        }
-        // Setting selected member to null
-        selectedMember.value = {
-            id: 0,
-            firstname: '',
-            lastname: '',
-            avatar: ''
-        }
+  if (selectedMember.value !== null) {
+    selectedSubteamCopy.members.push(selectedMember.value)
+    // Removing that member from the avaliable members drop down list
+    for (let i = 0; i < avaliableMemeberList.value.length; i++) {
+      if (avaliableMemeberList.value[i].id === selectedMember.value.id) {
+        avaliableMemeberList.value.splice(i, 1)
+      }
     }
+    // Setting selected member to null
+    selectedMember.value = {
+      id: 0,
+      firstname: '',
+      lastname: '',
+      avatar: ''
+    }
+  }
 }
 
 const removeMember = (memberId: number) => {
-  console.log(memberId)
   // Find the member to be removed
-  const foundMember: MemberView | undefined = selectedSubteamCopy.members.find((item) => item.id === memberId)
- 
+  const foundMember: MemberView | undefined = selectedSubteamCopy.members.find(
+    (item) => item.id === memberId
+  )
+
   // If found, add it to the avaliable member list and remove it from the selected subteam
-  console.log(foundMember)
   if (foundMember) {
-    avaliableMemeberList.value.push(foundMember);
+    avaliableMemeberList.value.push(foundMember)
     selectedSubteamCopy.members = selectedSubteamCopy.members.filter((item) => item.id !== memberId)
   }
 }
 
-const display = ref(0)
+const display = ref(false)
+const memberId = ref(0)
 
 const showDialog = computed({
-    // getter
-    get() {
-        return props.modelValue
-    },
-    // setter
-    set(newValue: boolean) {
-        // Note: we are using destructuring assignment syntax here.
-        emit('update:modelValue', newValue)
-    }
+  // getter
+  get() {
+    return props.modelValue
+  },
+  // setter
+  set(newValue: boolean) {
+    // Note: we are using destructuring assignment syntax here.
+    emit('update:modelValue', newValue)
+  }
 })
-
 </script>
 
 <template>
@@ -184,14 +187,19 @@ const showDialog = computed({
                 icon="mdi mdi-window-close"
                 size="24px"
                 id="pointer-cursor"
-                @click="display = member.id"
+                @click="
+                  () => {
+                    display = true
+                    memberId = member.id
+                  }
+                "
               />
               <PopupDialog
                 v-model="display"
                 :title="'Remove Team Member'"
                 :text="`Are you sure you wish to remove this member?`"
                 :submit-text="'Confirm'"
-                @handle-submit="removeMember(display)"
+                @handle-submit="removeMember(memberId)"
               />
             </v-list-item-content>
             <v-divider class="mt-4" color="info"></v-divider>
@@ -199,7 +207,13 @@ const showDialog = computed({
         </v-list>
         <!--Buttons-->
         <div class="w-100 d-flex justify-center">
-          <v-btn color="success" class="mr-8" variant="flat" @click="$emit('saveTeam', selectedSubteamCopy, avaliableMemeberList)">Save</v-btn>
+          <v-btn
+            color="success"
+            class="mr-8"
+            variant="flat"
+            @click="$emit('saveTeam', selectedSubteamCopy, avaliableMemeberList)"
+            >Save</v-btn
+          >
           <ConfirmButton
             :action="'delete'"
             :object="'subteam'"
