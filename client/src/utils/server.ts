050deb756c4e38142d90842188fc1864f1384d14
benvarian <88700858+benvarian@users.noreply.github.com>
2023-07-18 11:09:12 +0800

    lint

--- client/src/utils/server.ts
+++ client/src/utils/server.ts
@@ -2,14 +2,14 @@ import axios, { type AxiosRequestConfig } from 'axios'
 import { type Tokens } from '../types/user'
 
 const user = JSON.parse(<string>localStorage.getItem('authToken')) as Tokens
-let headers: AxiosRequestConfig['headers'] = {};
+let headers: AxiosRequestConfig['headers'] = {}
 
 if (user?.access) {
   headers = {
     Authorization: 'Bearer ' + user?.access
   }
 } else {
-    headers = {}
+  headers = {}
 }
 
 export default axios.create({ baseURL: import.meta.env.VITE_BACKEND_URL, headers })
