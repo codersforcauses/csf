ca2e9a1c36e839e20c00fba1b26c2fb6898f6ca8
benvarian <88700858+benvarian@users.noreply.github.com>
2023-07-18 15:55:13 +0800

    lint

--- server/api/team/test_unauthenticated.py
+++ server/api/team/test_unauthenticated.py
@@ -1,8 +1,8 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from api.users.models import User
 from api.team.models import Team
+
 
 class teamnoAuthTests(APITestCase):
     def setUp(self):
@@ -10,7 +10,7 @@ class teamnoAuthTests(APITestCase):
             name="mockTeam1",
             join_code="mockTeamCode1",
         )
-    
+
     def test_create_team(self):
         data = {'name': "ben", 'join_code': None, 'bio': None}
         response = self.client.post(reverse('team:create-team'), data, format='json')
@@ -40,5 +40,3 @@ class teamnoAuthTests(APITestCase):
         teamCountAfterDelete = Team.objects.all().count()
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(teamCountBeforeDelete, teamCountAfterDelete)
-
-    
