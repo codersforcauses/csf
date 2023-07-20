3f1f049cfd9e287b78f121bf5c7ca2df56a3ebbb
benvarian <88700858+benvarian@users.noreply.github.com>
2023-07-20 09:47:43 +0800

    maybe stuck

--- server/api/subteam/urls.py
+++ server/api/subteam/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = "subteam"
 urlpatterns = [
-    path("create_subteam/", views.create_subteam, name="create-subteam"),
+    path("create/", views.create_subteam, name="create-subteam"),
     path("get_subteams/<int:team_id>", views.get_subteams, name="get-subteams"),
     path("update_subteam/<int:subteam_id>", views.update_subteam, name="update-subteam"),
     path("delete_subteam/<int:subteam_id>", views.delete_subteam, name="delete-subteam"),
