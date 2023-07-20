9cc49dd1c4ffaeca446b3c6d5913457a4fc4ef37
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2023-07-20 13:12:42 +0800

    hotfix

--- client/src/utils/server.ts
+++ client/src/utils/server.ts
@@ -7,14 +7,13 @@ const server = axios.create({ baseURL: import.meta.env.VITE_BACKEND_URL })
 server.interceptors.request.use((config) => {
   const userStore = useUserStore()
   if (userStore.token != null)
-    config.headers = { Authorization: `Bearer ${userStore.token.access}`, ...config.headers }
+    config.headers = { ...config.headers, Authorization: 'Bearer ' + userStore.token.access }
   return config
 })
 
 server.interceptors.response.use(
   (r) => r,
   async (error: AxiosError & { config: { token_refreshed?: boolean } }) => {
-    console.log(error)
     if (error.response?.status == 401 && !error.config?.token_refreshed) {
       error.config.token_refreshed = true
       if (await useUserStore().refreshToken()) return server(error.config)
