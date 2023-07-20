00edd857a18787e38bf023986b13372223a0670c
YunnTun <22676427@student.uwa.edu.au>
2023-07-20 15:11:41 +0800

    added icons with 6 different sizes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -46,6 +46,42 @@ export default defineConfig({
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
+          },
+          {
+            src: 'images/CSF_feet_55_85px.png',
+            sizes: '55x85',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: 'images/CSF_feet_73_113px.png',
+            sizes: '73x113',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: 'images/CSF_feet_110_170px.png',
+            sizes: '110x170',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: 'images/CSF_feet_128_198px.png',
+            sizes: '128x198',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: 'images/CSF_feet_146_227px.png',
+            sizes: '146x227',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: 'images/CSF_feet_165_255px.png',
+            sizes: '165x255',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ]
       }
