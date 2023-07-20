3af34b79d0ca4e335de3ddcfaa45cded208b0849
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2023-07-20 10:15:06 +0800

    hotfix

--- server/api/subteam/urls.py
+++ server/api/subteam/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("get_subteams/<int:team_id>", views.get_subteams, name="get-subteams"),
     path("update_subteam/<int:subteam_id>", views.update_subteam, name="update-subteam"),
     path("delete_subteam/<int:subteam_id>", views.delete_subteam, name="delete-subteam"),
-    path("get_users/<int:subteam_id>", views.get_subteam_users, name="get-subteam-users"),
+    path("get_users/<int:subteam_id>", views.get_subteams, name="get-subteam-users"),
     path("get_available_users/", views.get_available_users, name="get-available-users"),
     path("edit_user/<int:subteam_id>/<int:user_id>", views.edit_user_to_subteam, name="edit-user"),
 
