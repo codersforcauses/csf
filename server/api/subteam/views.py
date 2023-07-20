38898eed5e50039065fe203f4d1837ac6ae70e85
benvarian <88700858+benvarian@users.noreply.github.com>
2023-07-20 09:49:45 +0800

    more stuff

--- server/api/subteam/views.py
+++ server/api/subteam/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from .models import SubTeam
 from .serializers import SubTeamSerialiser
 from ..users.models import User
-from ..users.serializers import UserSerialiser
+from ..users.serializers import UserSerialiser, UserUpdateSerializer
 
 from django.db.models import Q
 
@@ -54,5 +54,23 @@ def delete_subteam(request, subteam_id):
         return Response("User not authenticated", status=401)
     else:
         users = User.objects.filter(Q(subteam_id=subteam_id) & Q(team_id=request.user.team_id))
+        serialiser = UserUpdateSerializer(users, many=True)
+        return Response(serialiser.data, status=200)
+
+
+@api_view(['GET'])
+def get_available_users(request):
+    if request.user.is_authenticated is False:
+        return Response("User not authenticated", status=403)
+    else:
+        users = User.objects.filter(Q(subteam_id=None) & Q(team_id=request.user.team_id))
         serialiser = UserSerialiser(users, many=True)
         return Response(serialiser.data, status=200)
+
+
+@api_view(['PUT'])
+def edit_user_to_subteam(request, subteam_id, user_id):
+    user = User.objects.get(user_id=user_id)
+    user.subteam_id = subteam_id
+    user.save()
+    return Response("User added to subteam", status=200)
