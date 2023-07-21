6a307ce23b2da22f986793536fd0b2c73726cb8b
benvarian <88700858+benvarian@users.noreply.github.com>
2023-07-21 19:17:57 +0800

    change delete view to actually work

--- server/api/subteam/views.py
+++ server/api/subteam/views.py
@@ -27,6 +27,7 @@ def create_subteam(request):
 
 @api_view(['GET'])
 def get_subteams(request, team_id):
+    # todo add in auth checks for this endpoint
     subteams = SubTeam.objects.filter(team_id=team_id)
     serializer = SubTeamSerialiser(subteams, many=True)
     return Response(serializer.data, status=200)
@@ -54,9 +55,9 @@ def delete_subteam(request, subteam_id):
     if (request.user.is_authenticated is False):
         return Response("User not authenticated", status=401)
     else:
-        users = User.objects.filter(Q(subteam_id=subteam_id) & Q(team_id=request.user.team_id))
-        serialiser = UserUpdateSerializer(users, many=True)
-        return Response(serialiser.data, status=200)
+        subteam = SubTeam.objects.get(subteam_id=subteam_id)
+        subteam.delete()
+        return Response("subteam deleted", status=200)
 
 
 @api_view(['GET'])
