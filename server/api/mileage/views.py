b736383c8afbaa75aacddd0d75d35c9adfeec8ec
Arun Arjunan <arun.arjunan1@outlook.com>
2023-07-22 15:57:43 +0800

    llol

--- server/api/mileage/views.py
+++ server/api/mileage/views.py
@@ -1,4 +1,3 @@
-import logging
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
