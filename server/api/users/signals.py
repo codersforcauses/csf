f6296aec83bcdf553b69b9238f23869bd743d584
John Smith <makon1234@users.noreply.github.com>
2023-07-22 13:24:27 +0800

    reformatted code

--- server/api/users/signals.py
+++ server/api/users/signals.py
@@ -3,13 +3,17 @@ from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.conf import settings
 from api.users.models import User
+from django.core.mail import EmailMessage
 
 
 @receiver(post_save, sender=User)
 def send_new_user_to_admin(sender, instance, created, **kwargs):
     if created:
-        subject = 'A User has registered with CSF'
-        message = '{} has registered with CSF'.format(instance.first_name + " " + instance.last_name)
-        email_from = settings.EMAIL_ADDRESS_FROM
-        recipients = User.objects.filter(is_superuser=True).values_list('email', flat=True)
-        send_mail(subject, message, email_from, recipients)
+        email = EmailMessage(
+            'A User has registered with CSF',
+            '{} has registered with CSF'.format(instance.first_name + " " + instance.last_name),
+            settings.EMAIL_ADDRESS_FROM,
+            User.objects.filter(is_superuser=True).values_list('email', flat=True),
+        )
+        email.send()
+  
