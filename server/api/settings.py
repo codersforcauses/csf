e163a6f9bbaa84dc38c3dd2f2c8f1223d514ae0e
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2023-07-23 12:02:34 +0800

    Discard changes to server/api/settings.py

--- server/api/settings.py
+++ server/api/settings.py
@@ -12,12 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "root": {"level": "INFO"}
-}
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
