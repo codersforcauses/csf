44539bab63501e5d6dfbdb893da9f93d0c956d27
HuxleyBerry <huxley.berry@gmail.com>
2023-07-23 18:44:46 +0800

    wrap event title if it is long

--- client/src/components/EventCard.vue
+++ client/src/components/EventCard.vue
@@ -1,21 +1,17 @@
 <template>
   <v-card class="mx-3 elevation-0" :class="backgroundColour">
-    <v-card-title>
-      <span class="font-weight-bold" style="word-break: break-word">{{ event.name }}</span>
-      <v-spacer />
-      <v-icon
-        v-if="user?.teamAdmin && !event.isPublic"
-        icon="mdi-pencil"
-        @click="openModal"
-        size="24"
-        class="mr-2 mt-1"
-      />
-      <v-chip
-        variant="outlined"
-        :class="event.isPublic ? 'text-secondaryBlue' : 'text-secondaryGreen'"
-      >
-        {{ event.isPublic ? 'Official' : 'Private' }}
-      </v-chip>
+    <v-card-title style="word-break: break-word; overflow-wrap: break-word; white-space: normal">
+      <v-row no-gutters>
+        <span class="font-weight-bold">{{ event.name }}</span>
+        <v-icon icon="mdi-pencil" @click="openModal" size="24" class="mr-2 mt-1" />
+        <v-spacer />
+        <v-chip
+          variant="outlined"
+          :class="event.isPublic ? 'text-secondaryBlue' : 'text-secondaryGreen'"
+        >
+          {{ event.isPublic ? 'Official' : 'Private' }}
+        </v-chip>
+      </v-row>
     </v-card-title>
     <v-card-subtitle class="text-primaryRed font-italic"
       >{{ event.startDate }} - {{ event.endDate }}</v-card-subtitle
