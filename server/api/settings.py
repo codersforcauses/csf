ac068378b8a4e56f76a508c860c9e037d5102403
zhanerken <zhanerken@gmail.com>
2023-07-24 00:01:45 +0800

    linting fixes

--- server/api/settings.py
+++ server/api/settings.py
@@ -185,13 +185,8 @@ REST_FRAMEWORK = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_ADDRESS_FROM = os.environ.get("EMAIL_ADDRESS_FROM")
-EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_API_KEY')
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_PASSWORD = os.getenv("SENDGRID_API_KEY")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_USE_TLS = True
-
-
-
-
-
