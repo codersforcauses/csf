b62cec3404c2756c82745246a4556ba377044647
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2023-07-25 18:33:15 +0800

    fix: Total_mileage is a required field for team and user

--- server/api/team/models.py
+++ server/api/team/models.py
@@ -7,7 +7,7 @@ class Team(models.Model):
     name = models.CharField(unique=True, max_length=254)
     join_code = models.CharField(unique=True, default=None)
     bio = models.TextField(default="")
-    total_mileage = models.FloatField(default=0.0)
+    total_mileage = models.FloatField(default=0.0, null=True, blank=True)
 
     def __str__(self):
         return f"{self.name} - {self.join_code}"
