42ad521b56b6231b82a0a4c46c7c0608972b7298
ChantelleKerr <chantellekerr93@gmail.com>
2023-07-25 13:20:29 +0800

    comment email out until prod email works

--- server/api/users/signals.py
+++ server/api/users/signals.py
@@ -4,14 +4,14 @@ from django.conf import settings
 from api.users.models import User
 from django.core.mail import EmailMessage
 
-
-@receiver(post_save, sender=User)
-def send_new_user_to_admin(sender, instance, created, **kwargs):
-    if created:
-        email = EmailMessage(
-            'Stride For Education: New User Registered',
-            '{} has registered an account on Stride For Education'.format(instance.first_name + " " + instance.last_name),
-            settings.EMAIL_ADDRESS_FROM,
-            [settings.EMAIL_ADDRESS_FROM],
-        )
-        email.send()
+# Comment out until working for production
+# @receiver(post_save, sender=User)
+# def send_new_user_to_admin(sender, instance, created, **kwargs):
+#     if created:
+#         email = EmailMessage(
+#             'Stride For Education: New User Registered',
+#             '{} has registered an account on Stride For Education'.format(instance.first_name + " " + instance.last_name),
+#             settings.EMAIL_ADDRESS_FROM,
+#             [settings.EMAIL_ADDRESS_FROM],
+#         )
+#         email.send()
