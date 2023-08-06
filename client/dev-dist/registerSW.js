a395a198a2d75a67572dbc2f59e317f57fed4411
benvarian <88700858+benvarian@users.noreply.github.com>
2023-08-06 17:32:20 +0800

    Revert "hopefully fixed problem"
    This reverts commit 9ecff5b427f69396efb5d2226b3e62c1b72e6b2e.

--- client/dev-dist/registerSW.js
+++ client/dev-dist/registerSW.js
@@ -1,8 +1,3 @@
 if ('serviceWorker' in navigator) {
-  await navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' }).then(() => {
-    console.log("Service Worker Registered");
-  }).catch((error) => {
-    console.log(error);
-  })
-  await navigator.serviceWorker.ready;
+  navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
 }
