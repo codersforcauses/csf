26c82984e17b2202d749af33cb84b45169e8ae24
benvarian <88700858+benvarian@users.noreply.github.com>
2023-08-06 17:32:16 +0800

    Revert "prettier"
    This reverts commit ff81453aa048170763d5e6c447df94c1f439e66e.

--- client/dev-dist/registerSW.js
+++ client/dev-dist/registerSW.js
@@ -1,11 +1,8 @@
 if ('serviceWorker' in navigator) {
-  await navigator.serviceWorker
-    .register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
-    .then(() => {
-      console.log('Service Worker Registered')
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  await navigator.serviceWorker.ready
+  await navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' }).then(() => {
+    console.log("Service Worker Registered");
+  }).catch((error) => {
+    console.log(error);
+  })
+  await navigator.serviceWorker.ready;
 }
